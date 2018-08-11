abb623a190b26c34efc8c72d49f17ec536fcd612
monteiro-bruno <31869510+monteiro-bruno@users.noreply.github.com>
2018-08-10 21:48:06 -0300

    Update Kosaraju.cpp

--- Codigo/Grafos/Kosaraju.cpp
+++ Codigo/Grafos/Kosaraju.cpp
@@ -1,29 +1,37 @@
+// Kosaraju
+//
+// O(n + m)
+
+int n;
+vector<vector<int> > g(MAX);
+vector<vector<int> > gi(MAX); // grafo invertido
+int vis[MAX];
+stack<int> S;
+int comp[MAX];                // componente conexo de cada vertice
+
 void dfs(int k) {
 	vis[k] = 1;
-	for (int i = 0; i < (int) G[k].size(); i++)
-		if (!vis[G[k][i].first]) dfs(G[k][i].first);
+	for (int i = 0; i < (int) g[k].size(); i++)
+		if (!vis[g[k][i]]) dfs(g[k][i]);
+
 	S.push(k);
 }
 
-void scc(int k) {
+void scc(int k, int c) {
 	vis[k] = 1;
-	for (int i = 0; i < (int) GI[k].size(); i++)
-		if (!vis[GI[k][i].first]) {
-			une(k, GI[k][i].first);
-			scc(GI[k][i].first);
-		}
+	comp[k] = c;
+	for (int i = 0; i < (int) gi[k].size(); i++)
+		if (!vis[gi[k][i]]) scc(gi[k][i], c);
 }
 
 void kosaraju() {
 	for (int i = 0; i < n; i++) vis[i] = 0;
 	for (int i = 0; i < n; i++) if (!vis[i]) dfs(i);
 
-	build();
-
 	for (int i = 0; i < n; i++) vis[i] = 0;
 	while (S.size()) {
 		int u = S.top();
 		S.pop();
-		if (!vis[u]) scc(u);
+		if (!vis[u]) scc(u, u);
 	}
 }

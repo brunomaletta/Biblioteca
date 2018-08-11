94a273e50423d56aacc7c043cf4b001c873ca870
monteiro-bruno <31869510+monteiro-bruno@users.noreply.github.com>
2018-08-10 22:18:03 -0300

    Update mdcExtendido.cpp

--- Codigo/Matematica/mdcExtendido.cpp
+++ Codigo/Matematica/mdcExtendido.cpp
@@ -1,4 +1,9 @@
-//ax + by = mdc(a, b)
+// Algoritmo de Euclides extendido
+//
+// acha x e y tal que ax + by = mdc(a, b)
+//
+// O(log(min(a, b)))
+
 int mdce(int a, int b, int *x, int *y){
 	if(!a){
 		*x = 0;
@@ -7,8 +12,8 @@ int mdce(int a, int b, int *x, int *y){
 	}
 
 	int X, Y;
-	int mdc = mdce(b%a, a, &X, &Y);
-	*x = Y - (b/a)*X;
+	int mdc = mdce(b % a, a, &X, &Y);
+	*x = Y - (b / a) * X;
 	*y = X;
 
 	return mdc;

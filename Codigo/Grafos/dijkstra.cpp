8f5744b0c177f99cd2acb51bc4f74542a5f9566d
monteiro-bruno <31869510+monteiro-bruno@users.noreply.github.com>
2018-08-10 23:58:49 -0300

    Create dijkstra.cpp

---
+++ Codigo/Grafos/dijkstra.cpp
@@ -0,0 +1,33 @@
+// Dijkstra
+//
+// encontra menor distancia de a
+// para todos os vertices
+// se ao final do algoritmo d[i] = INF,
+// entao a nao alcanca i
+//
+// O(m log(n))
+
+int n;
+vector<vector<int> > g(MAX);
+vector<vector<int> > w(MAX); // peso das arestas
+int d[MAX];
+
+void dijsktra(int a) {
+	for (int i = 0; i < n; i++) d[i] = INF;
+	d[a] = 0;
+	priority_queue<pair<int, int> > Q;
+	Q.push(make_pair(0, a));
+
+	while (Q.size()) {
+		int u = Q.top().second;
+		Q.pop();
+
+		for (int i = 0; i < (int) g[u].size(); i++) {
+			int v = g[u][i];
+			if (d[v] > d[u] + w[u][i]) {
+				d[v] = d[u] + w[u][i];
+				Q.push(make_pair(-d[v], v));
+			}
+		}
+	}
+}

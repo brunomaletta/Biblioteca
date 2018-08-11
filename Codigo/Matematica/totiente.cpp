86fe37a2ab394baf715e998d53e12c3f1367b41b
monteiro-bruno <31869510+monteiro-bruno@users.noreply.github.com>
2018-08-10 22:41:16 -0300

    Update and rename phi.cpp to totiente.cpp

---
+++ Codigo/Matematica/totiente.cpp
@@ -0,0 +1,17 @@
+// Funcao totiente de Euler
+//
+// O(sqrt(n))
+
+int tot(int n){
+	int ret = n;
+
+	for (int i = 2; i * i <= n; i++)
+		if (n % i == 0) {
+			while (n % i == 0) n /= i;
+			ret -= ret / i;
+		}
+
+	if(n > 1) ret -= ret / n;
+
+	return ret;
+}

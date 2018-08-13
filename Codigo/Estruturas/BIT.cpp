9225428b60f2e97b40759d625a2bce169a2f2685
monteiro-bruno <31869510+monteiro-bruno@users.noreply.github.com>
2018-08-13 11:14:25 -0300

    Update BIT.cpp

--- Codigo/Estruturas/BIT.cpp
+++ Codigo/Estruturas/BIT.cpp
@@ -1,17 +1,26 @@
 // BIT de soma
 //
-// BIT 1-based
+// BIT 1-based, v 0-based
 // Para mudar o valor da posicao p para x,
 // faça: poe(x - sum(p, p), p)
 //
 // Complexidades:
-// build - O(n log(n))
+// build - O(n)
 // poe - O(log(n))
 // query - O(log(n))
 
 int n;
 int bit[MAX];
 int v[MAX];
+
+void build() {
+	for (int i = 1; i <= n; i++) bit[i] = v[i - 1];
+
+	for (int i = 1; i <= n; i++) {
+		int j = i + (i & -i);
+		if (j <= n) bit[j] += bit[i];
+	}
+}
 
 // soma x na posicao p
 void poe(int x, int p) {
@@ -35,10 +44,4 @@ int query(int p) {
 int sum(int a, int b) {
 	if (a == 1) return query(b);
 	return query(b) - query(a - 1);
-}
-
-void build() {
-	for (int i = 1; i <= n; i++) bit[i] = 0;
-
-	for (int i = 1; i <= n; i++) poe(v[i - 1], i);
 }

032bad3da86efff588b9894d97015fea21976c5e
monteiro-bruno <31869510+monteiro-bruno@users.noreply.github.com>
2018-08-23 18:43:51 -0300

    Update mdc.cpp

--- Codigo/Matematica/mdc.cpp
+++ Codigo/Matematica/mdc.cpp
@@ -1,7 +1,7 @@
 // Algoritmo de Euclides
 //
 // O(log(min(a, b)))
-// Na prática, pode ser considerado O(1)
+// Na pratica, pode ser considerado O(1)
 
 int mdc(int a, int b) {
 	return !b ? a : mdc(b, a % b);

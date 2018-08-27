67b0f2bff532e3c08dfe49d1ebf193d1f5b17560
monteiro-bruno <31869510+monteiro-bruno@users.noreply.github.com>
2018-08-26 21:58:49 -0300

    Update dinic.cpp

--- Codigo/Grafos/dinic.cpp
+++ Codigo/Grafos/dinic.cpp
@@ -1,23 +1,23 @@
-//Algoritmo de Dinic para fluxo maximo
+// Algoritmo de Dinic para fluxo maximo
 //
-//tem que definir o tamanho de g e de lev como o numero
-//de vertices do grafo e depois char o a funcao fluxo
+// tem que definir o tamanho de g e de lev como o numero
+// de vertices do grafo e depois char o a funcao fluxo
 //
-//Complexidade:
-//Caso geral: O(E^2 * E)
-//Grafo bipartido O(sqrt(V)*E)
+// Complexidade:
+// Caso geral: O(E^2 * E)
+// Grafo bipartido O(sqrt(V)*E)
 
 struct edge{
-	int p, c, id; //destino, capacidade, id
+	int p, c, id; // destino, capacidade, id
 	edge() {p = c = id = 0;}
 	edge(int p, int c, int id):p(p), c(c), id(id){}
 };
 
-vector<vector<edge> > g; //define o tamanho depois
+vector<vector<edge> > g; // define o tamanho depois
 vector <int> lev;
 
 void add(int a, int b, int c){
-	//de a para b com capacidade c
+	// de a para b com capacidade c
 	edge d = {b, c, (int) g[b].size()};
 	edge e = {a, 0, (int) g[a].size()};
 	g[a].pb(d);
@@ -25,12 +25,12 @@ void add(int a, int b, int c){
 }
 
 bool bfs(int s, int t){
-	//bfs de s para t construindo o level
+	// bfs de s para t construindo o level
 	for(int i = 0; i < g.size(); i++)
 		lev[i] = -1;
 	lev[s] = 0;
 
-	//bfs saindo de s
+	// bfs saindo de s
 	queue <int> q;
 	q.push(s);
 	while(q.size()){
@@ -38,7 +38,7 @@ bool bfs(int s, int t){
 
 		for(int i = 0; i < g[u].size(); i++){
 			edge e = g[u][i];
-			//se ja foi visitado ou nao tem capacidade nao visita
+			// se ja foi visitado ou nao tem capacidade nao visita
 			if(lev[e.p] != -1 || !e.c)
 				continue;
 			lev[e.p] = lev[u] + 1;
@@ -59,12 +59,12 @@ int dfs(int v, int s, int f){
 	for(int i = 0; i < g[v].size(); i++){
 		edge e = g[v][i]; int u = e.p;
 
-		//visita se tiver capacidadade e se ta no proximo nivel
+		// visita se tiver capacidadade e se ta no proximo nivel
 		if(lev[u] != lev[v] + 1 || !e.c)
 			continue;
 
 		int tenta = dfs(u, s, min(flu, e.c));
-		//se passou alguma coisa altera as capacidades
+		// se passou alguma coisa altera as capacidades
 		if(tenta){
 			flu -= tenta;
 			g[v][i].c -= tenta;
@@ -72,7 +72,7 @@ int dfs(int v, int s, int f){
 		}
 	}
 
-	//se passou tudo tira da lista dos possiveis
+	// se passou tudo tira da lista dos possiveis
 	if(flu == f)
 		lev[v] = -1;
 	return f - flu;
@@ -85,7 +85,7 @@ int fluxo(int s, int t){
 	return r;
 }
 
-//ja tem ate o debug
+// ja tem ate o debug
 void imprime(){
 	for(int i = 0; i < g.size(); i++){
 		printf("%i -> ", i);

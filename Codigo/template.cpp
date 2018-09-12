73228e19bf2a91c283051a3a13f3e2c87b9286fc
monteiro-bruno <31869510+monteiro-bruno@users.noreply.github.com>
2018-09-12 20:28:39 -0300

    Create template.cpp

---
+++ Codigo/template.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define sc(a) scanf("%d",&a)
+#define sc2(a,b) sc(a), sc(b)
+#define sc3(a,b,c) s2(a, b), sc(c)
+#define pri(x) printf("%d\n",x)
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define BUFF ios::sync_with_stdio(0)
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ii> vii;
+
+const int INF = 0x3f3f3f3f;
+
+int main() {
+	
+	return 0;
+}

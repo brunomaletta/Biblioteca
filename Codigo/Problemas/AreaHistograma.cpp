17b929fe2519db1f1d98b77031b7692986e49875
monteiro-bruno <31869510+monteiro-bruno@users.noreply.github.com>
2018-09-23 18:48:38 -0300

    Create AreaHistograma.cpp

---
+++ Codigo/Problemas/AreaHistograma.cpp
@@ -0,0 +1,27 @@
+// Area Maxima de Histograma
+//
+// Assume que todas as barras tem largura 1,
+// e altura dada no vetor v
+//
+// O(n)
+
+typedef long long ll;
+
+ll area(vector<int> v) {
+	ll ret = 0;
+	stack<int> s;
+	// valores inicial pra dar tudo certo
+	v.insert(v.begin(), -1);
+	v.insert(v.end(), -1);
+	s.push(0);
+
+	for(int i = 0; i < (int) v.size(); i++) {
+		while (v[s.top()] > v[i]) {
+			ll h = v[s.top()]; s.pop();
+			ret = max(ret, h * (i - s.top() - 1));
+		}
+		s.push(i);
+	}
+  
+	return ret;
+}

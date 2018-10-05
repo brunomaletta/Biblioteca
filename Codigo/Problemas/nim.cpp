e3665c43c1d948a60ebb2f0d6cf6d268ee700f6c
monteiro-bruno <31869510+monteiro-bruno@users.noreply.github.com>
2018-10-05 19:15:01 -0300

    Update nim.cpp

--- Codigo/Problemas/nim.cpp
+++ Codigo/Problemas/nim.cpp
@@ -4,27 +4,46 @@
 // Assume que o estado atual eh perdedor
 // Funcao move retorna um par com a pilha (0 indexed)
 // e quanto deve ser tirado dela
+// XOR deve estar armazenado em x
+// Para mudar um valor, faca insere(novo_valor),
+// atualize o XOR e mude o valor em v
 //
 // MAX2 = teto do log do maior elemento
 // possivel nas pilhas
 //
-// O(n)
+// O(log(n)) amortizado
+ 
+int v[MAX], n, x;
+stack<int> pi[MAX2];
+ 
+void insere(int p) {
+    for (int i = 0; i < MAX2; i++) if (v[p] & (1 << i)) pi[i].push(p);
+}
+ 
+pair<int, int> move() {
+    int bit = 0; while (x >> bit) bit++; bit--;
+   
+    // tira os caras invalidos
+    while ((v[pi[bit].top()] & (1 << bit)) == 0) pi[bit].pop();
+ 
+    int cara = pi[bit].top();
+    int tirei = v[cara] - (x^v[cara]);
+    v[cara] -= tirei;
+ 
+    insere(cara);
+ 
+    return mp(cara, tirei);
+}
 
-int v[MAX], n;
+// Acha o movimento otimo baseado
+// em v apenas
+//
+// O(n)
 
 pair<int, int> move() {
 	int x = 0;
-	for (int j = 0; j < n; j++) x ^= v[j];
+	for (int i = 0; i < n; i++) x ^= v[i];
 
-	int p = -1;
-	for (int i = 1 << MAX2; i; i >>= 1) if (x & i) {
-		for (int j = 0; j < n; j++) if (v[j] & i) {
-			p = j;
-			break;
-		}
-		break;
-	}
-
-	x ^= v[p];
-	return make_pair(p, v[p] - x);
+	for (int i = 0; i < n; i++) if ((v[i]^x) < v[i])
+		return mp(i, v[i] - (v[i]^x));
 }

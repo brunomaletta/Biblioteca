a0d67540036aa54b24ff0f86f73dfeb679ff02f1
monteiro-bruno <31869510+monteiro-bruno@users.noreply.github.com>
2018-10-11 21:15:52 -0300

    Update tarjan.cpp

--- Codigo/Grafos/tarjan.cpp
+++ Codigo/Grafos/tarjan.cpp
@@ -4,18 +4,18 @@
 
 int n;
 vector<vector<int> > g(MAX);
-stack<int> S;
+stack<int> s;
 int vis[MAX], comp[MAX];
 int id[MAX], p;
 
 int dfs(int k) {
 	int lo = id[k] = p++;
-	S.push(k);
+	s.push(k);
 	vis[k] = 2; // ta na pilha
 
 	// calcula o menor cara q ele alcanca
 	// que ainda nao esta em um scc
-	for (int i = 0; i < sz(g[k]); i++) {
+	for (int i = 0; i < g[k].size(); i++) {
 	 	if (!vis[g[k][i]])
 			lo = min(lo, dfs(g[k][i]));
 		else if (vis[g[k][i]] == 2)
@@ -24,8 +24,8 @@ int dfs(int k) {
 
 	// nao alcanca ninguem menor -> comeca scc
 	if (lo == id[k]) while (1) {
-		int u = S.top();
-		S.pop(); vis[u] = 1;
+		int u = s.top();
+		s.pop(); vis[u] = 1;
 		comp[u] = k;
 		if (u == k) break;
 	}

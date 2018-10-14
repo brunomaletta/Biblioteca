fc1ee8adb2e3ad307e7d87ad086b1b525f175439
brunomaletta <31869510+brunomaletta@users.noreply.github.com>
2018-10-14 15:03:19 -0300

    Update SegTree2D.cpp

--- Codigo/Estruturas/SegTree2D.cpp
+++ Codigo/Estruturas/SegTree2D.cpp
@@ -19,45 +19,32 @@ int n;
 
 void build() {
 	// constroi as folhas de cada seg-tree
-	for (int x = n - 1; x; x--)
-		for (int y = n; y < 2*n; y++)
-			seg[x][y] = seg[2*x][y] + seg[2*x+1][y];
+	for (int x = n - 1; x; x--) for (int y = n; y < 2*n; y++)
+		seg[x][y] = seg[2*x][y] + seg[2*x+1][y];
 	
 	// constroi todas as seg-trees
-	for (int x = 0; x < 2*n; x++)
-		for (int y = n - 1; y; y--)
-			seg[x][y] = seg[x][2*y] + seg[x][2*y+1];
+	for (int x = 0; x < 2*n; x++) for (int y = n - 1; y; y--)
+		seg[x][y] = seg[x][2*y] + seg[x][2*y+1];
 }
 
 
 int query(int x1, int y1, int x2, int y2) {
 	int ret = 0;
-	int y1_ = y1 + n, y2_ = y2 + n;
-	for (x1 += n, x2 += n; x1 <= x2; x1 /= 2, x2 /= 2) {
-		if (x1 % 2 == 1) {
-			y1 = y1_, y2 = y2_;
-			for (; y1 <= y2; y1 /= 2, y2 /= 2) {
-				if (y1 % 2 == 1) ret += seg[x1][y1++];
-				if (y2 % 2 == 0) ret += seg[x1][y2--];
-			}
-			x1++;
+	int y3 = y1 + n, y4 = y2 + n;
+	for (x1 += n, x2 += n; x1 <= x2; ++x1 /= 2, --x2 /= 2)
+		for (y1 = y3, y2 = y4; y1 <= y2; ++y1 /= 2, --y2 /= 2) {
+			if (x1%2 == 1 and y1%2 == 1) ret += seg[x1][y1];
+			if (x1%2 == 1 and y2%2 == 0) ret += seg[x1][y2];
+			if (x2%2 == 0 and y1%2 == 1) ret += seg[x2][y1];
+			if (x2%2 == 0 and y2%2 == 0) ret += seg[x2][y2];
 		}
-		if (x2 % 2 == 0) {
-			y1 = y1_, y2 = y2_;
-			for (; y1 <= y2; y1 /= 2, y2 /= 2) {
-				if (y1 % 2 == 1) ret += seg[x2][y1++];
-				if (y2 % 2 == 0) ret += seg[x2][y2--];
-			}
-			x2--;
-		}
-	}
+
 	return ret;
 }
 
 void update(int x, int y, int val) {
-	int y_ = y + n;
-	for (x += n; x; x /= 2) {
-		y = y_;
+	int y2 = y += n;
+	for (x += n; x; x /= 2, y = y2) {
 		// atualiza folha da seg-tree
 		if (x >= n) seg[x][y] = val;
 		else seg[x][y] = seg[2*x][y] + seg[2*x+1][y];

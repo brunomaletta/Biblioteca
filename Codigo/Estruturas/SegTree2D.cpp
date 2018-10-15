9a145662d3a602024daee20642529651da0d2e74
brunomaletta <31869510+brunomaletta@users.noreply.github.com>
2018-10-15 15:20:45 -0300

    Update SegTree2D.cpp

--- Codigo/Estruturas/SegTree2D.cpp
+++ Codigo/Estruturas/SegTree2D.cpp
@@ -4,6 +4,7 @@
 // Um valor inicial em (x, y) deve ser colocado em seg[x+n][y+n]
 // Query: soma do retangulo ((x1, y1), (x2, y2))
 // Update: muda o valor da posicao (x, y) para val
+// Não pergunte como que essa coisa funciona
 //
 // Para query com distancia de manhattan <= d, faca
 // nx = x+y, ny = x-y
@@ -18,20 +19,16 @@ int seg[2*MAX][2*MAX];
 int n;
 
 void build() {
-	// constroi as folhas de cada seg-tree
-	for (int x = n - 1; x; x--) for (int y = n; y < 2*n; y++)
-		seg[x][y] = seg[2*x][y] + seg[2*x+1][y];
-	
-	// constroi todas as seg-trees
-	for (int x = 0; x < 2*n; x++) for (int y = n - 1; y; y--)
-		seg[x][y] = seg[x][2*y] + seg[x][2*y+1];
+	for (int x = 2*n; x; x--) for (int y = 2*n; y; y--) {
+		if (x < n) seg[x][y] = seg[2*x][y] + seg[2*x+1][y];
+		if (y < n) seg[x][y] = seg[x][2*y] + seg[x][2*y+1];
+	}
 }
 
 
 int query(int x1, int y1, int x2, int y2) {
 	int ret = 0;
 	int y3 = y1 + n, y4 = y2 + n;
-	// confia que funciona
 	for (x1 += n, x2 += n; x1 <= x2; ++x1 /= 2, --x2 /= 2)
 		for (y1 = y3, y2 = y4; y1 <= y2; ++y1 /= 2, --y2 /= 2) {
 			if (x1%2 == 1 and y1%2 == 1) ret += seg[x1][y1];
@@ -46,11 +43,9 @@ int query(int x1, int y1, int x2, int y2) {
 void update(int x, int y, int val) {
 	int y2 = y += n;
 	for (x += n; x; x /= 2, y = y2) {
-		// atualiza folha da seg-tree
 		if (x >= n) seg[x][y] = val;
 		else seg[x][y] = seg[2*x][y] + seg[2*x+1][y];
 		
-		// propaga para cima
 		while (y /= 2) seg[x][y] = seg[x][2*y] + seg[x][2*y+1];
 	}
 }

dc5b90dd307141f13b50fbbc0609f21086afb72a
brunomaletta <31869510+brunomaletta@users.noreply.github.com>
2018-12-05 18:34:38 -0200

    Update totiente.cpp

--- Codigo/Matematica/totiente.cpp
+++ Codigo/Matematica/totiente.cpp
@@ -5,13 +5,11 @@
 int tot(int n){
 	int ret = n;
 
-	for (int i = 2; i*i <= n; i++)
-		if (n % i == 0) {
-			while (n % i == 0) n /= i;
-			ret -= ret / i;
-		}
-
-	if(n > 1) ret -= ret / n;
+	for (int i = 2; i*i <= n; i++) if (n % i == 0) {
+		while (n % i == 0) n /= i;
+		ret -= ret / i;
+	}
+	if (n > 1) ret -= ret / n;
 
 	return ret;
 }

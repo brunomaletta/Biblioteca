fb14a62d84bff8c81d73f7d685177dc98e85f566
brunomaletta <31869510+brunomaletta@users.noreply.github.com>
2018-12-05 18:37:43 -0200

    Update crivo.cpp

--- Codigo/Matematica/crivo.cpp
+++ Codigo/Matematica/crivo.cpp
@@ -1,14 +1,63 @@
-// Crivo de Eratosthenes
+// Variacoes do crivo de Eratosthenes
+
+// "O" crivo
+//
+// Encontra maior divisor primo
+// Um numero eh primo sse div[x] == x
+// fact fatora um numero <= lim
+// A fatoracao sai ordenada
+//
+// crivo - O(n log(log(lim)))
+// fact - O(log(n))
+
+int divi[MAX];
+
+void crivo(int lim) {
+	for (int i = 1; i <= lim; i++) divi[i] = 1;
+
+	for (int i = 2; i <= lim; i++) if (divi[i] == 1)
+		for (int j = i; j <= lim; j += i) divi[j] = i;
+}
+
+void fact(vector<int>& v, int n) {
+	if (n != divi[n]) fact(v, n/divi[n]);
+	v.push_back(divi[n]);
+}
+
+// Crivo de divisores
+//
+// Encontra numero de divisores
+// ou soma dos divisores
+//
+// O(n log(n))
+
+int divi[MAX];
+
+void crivo(int lim) {
+	for (int i = 1; i <= lim; i++) divi[i] = 1;
+
+	for (int i = 2; i <= lim; i++)
+		for (int j = i; j <= lim; j += i) {
+			// para numero de divisores
+			divi[j]++;
+			// para soma dos divisores
+			divi[j] += i;
+		}
+}
+
+// Crivo de totiente
+//
+// Encontra o valor da funcao
+// totiente de Euler
 //
 // O(n log(log(n)))
 
-int primo[MAX];
-int n;
+int tot[MAX];
 
-void crivo() {
-	primo[1] = 0;
-	for (int i = 2; i <= n; i++) primo[i] = 1;
+void crivo(int lim) {
+	for (int i = 1; i <= lim; i++) tot[i] = i;
 
-	for (int i = 2; i*i <= n; i++) if (primo[i])
-		for (int j = i*i; j <= n; j += i) primo[j] = 0;
+	for (int i = 2; i <= lim; i++) if (tot[i] == i)
+		for (int j = i; j <= lim; j += i)
+			tot[j] -= tot[j] / i;
 }

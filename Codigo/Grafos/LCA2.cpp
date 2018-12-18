e14315765ebc58c6880b2e6e09626dfc05889a15
brunomaletta <31869510+brunomaletta@users.noreply.github.com>
2018-12-18 11:46:43 -0200

    Update LCA2.cpp

--- Codigo/Grafos/LCA2.cpp
+++ Codigo/Grafos/LCA2.cpp
@@ -12,7 +12,7 @@ vector<vector<int> > g(MAX);
 int in[MAX], h[MAX], sz[MAX];
 int pai[MAX], t;
 
-void build(int k, int p = -1, int f = 0) {
+void build(int k, int p = -1, int f = 1) {
 	in[k] = t++; sz[k] = 1;
 	for (int& i : g[k]) if (i != p) {
 		h[i] = (i == g[k][0] ? h[k] : i);
@@ -20,7 +20,7 @@ void build(int k, int p = -1, int f = 0) {
 		sz[k] += sz[i]; pai[i] = k;
 		if (sz[i] > sz[g[k][0]]) swap(i, g[k][0]);
 	}
-	if (p*!f == -1) t = 0, h[k] = k, build(k, -1, 1);
+	if (p*f == -1) t = 0, h[k] = k, build(k, -1, 0);
 }
 
 

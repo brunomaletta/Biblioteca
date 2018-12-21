20c044c38866c84d9db1a53f4813e5ebee0de032
brunomaletta <31869510+brunomaletta@users.noreply.github.com>
2018-12-21 19:40:26 -0200

    Update SegTree.cpp

--- Codigo/Estruturas/SegTree.cpp
+++ Codigo/Estruturas/SegTree.cpp
@@ -1,78 +1,49 @@
 // SegTree recursiva com Lazy Propagation
 //
-// SegTree 1-based, vetor 0-based
-// Query: soma do range [a, b], 1-based
-// Update: soma x em cada elemento do range [a, b], 1-based
+// Query: soma do range [a, b]
+// Update: soma x em cada elemento do range [a, b]
 //
 // Complexidades:
 // build - O(n)
 // query - O(log(n))
 // update - O(log(n))
 
-int seg[4 * MAX];
-int lazy[4 * MAX];
+int seg[4*MAX];
+int lazy[4*MAX];
 int v[MAX];
 int n, a, b, x;
 
 int build(int p, int l, int r) {
 	lazy[p] = 0;
-	if (l == r) return seg[p] = v[l - 1];
+	if (l == r) return seg[p] = v[l];
 
-	int m = (l + r) / 2;
-	return seg[p] = build(2 * p, l, m) + build(2 * p + 1, m + 1, r);
+	int m = (l+r)/2;
+	return seg[p] = build(2*p, l, m) + build(2*p+1, m+1, r);
 }
 
-// propagar o que ta em p para os filhos
 void prop(int p, int l, int r) {
-	// soma o lazy
-	seg[p] += lazy[p] * (r - l + 1);
-
-	// propaga pros filhos
-	if (l != r) {
-		lazy[2 * p] += lazy[p];
-		lazy[2 * p + 1] += lazy[p];
-	}
-
-	// zera o lazy
+	seg[p] += lazy[p] * (r-l+1);
+	if (l != r) lazy[2*p] += x, lazy[2*p+1] += x;
 	lazy[p] = 0;
 }
 
-// somar x no intervalo [l, r]
-int lazy_op(int p, int l, int r) {
-	// soma x * (tamanho do intervalo)
-	seg[p] += x * (r - l + 1);
-
-	// suja os filhos
-	if (l != r) {
-		lazy[2 * p] += x;
-		lazy[2 * p + 1] += x;
-	}
-
-	return seg[p];
-}
-
 int query(int p, int l, int r) {
-	// propaga
 	prop(p, l, r);
-
-	// to totalmente dentro
 	if (a <= l and r <= b) return seg[p];
-	// to fora
 	if (b < l or r < a) return 0;
 
-	int m = (l + r) / 2;
+	int m = (l+r)/2;
 	return query(2 * p, l, m) + query(2 * p + 1, m + 1, r);
 }
 
 int update(int p, int l, int r) {
-	// propaga
 	prop(p, l, r);
-
-	// to totalmente dentro
-	if (a <= l and r <= b) return lazy_op(p, l, r);
-	// to fora
+	if (a <= l and r <= b) {
+		if (l != r) lazy[2*p] += x, lazy[2*p+1] += x;
+		return seg[p] += x * (r-l+1);
+	}
 	if (b < l or r < a) return seg[p];
 
-	int m = (l + r) / 2;
-	return seg[p] = update(2 * p, l, m) + update(2 * p + 1, m + 1, r);
+	int m = (l+r)/2;
+	return seg[p] = update(2*p, l, m) + update(2*p+1, m+1, r);
 }

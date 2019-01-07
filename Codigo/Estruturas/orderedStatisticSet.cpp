c1069b7125d6c9aeaf5bedbcce1f62d1e38431db
brunomaletta <31869510+brunomaletta@users.noreply.github.com>
2019-01-07 10:53:18 -0200

    Create orderedStatisticSet.cpp

---
+++ Codigo/Estruturas/orderedStatisticSet.cpp
@@ -0,0 +1,27 @@
+// Order Statistic Set do GCC
+//
+// Funciona do C++11 pra cima
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <class T>
+	using ord_set = tree<T, null_type, less<T>, rb_tree_tag,
+	tree_order_statistics_node_update>;
+	
+// para declarar:
+ord_set<int> s;
+// coisas do set normal funcionam:
+for (auto i : s) cout << i << endl;
+cout << s.size() << endl;
+// k-esimo maior elemento O(log|s|):
+cout << *s.find_by_order(k) << endl;
+// quantos sao menors do que k O(log|s|):
+cout << s.order_of_key(k) << endl;
+
+// Para fazer um multiset, tem que
+// usar ord_set<pair<int, int> > com o
+// segundo parametro sendo algo para diferenciar
+// os ementos iguais.
+// s.order_of_key({k, -INF}) vai retornar o
+// numero de elementos < k

78a24b8babb4c517e5033a09ae6af1ee0251f997
Bruno <malettabruno@gmail.com>
2019-11-23 00:46:12 -0300

    Go

--- Codigo/Grafos/linetree.cpp
+++ Codigo/Grafos/linetree.cpp
@@ -23,17 +23,13 @@ namespace linetree {
 			int a = id[i.second.first], b = id[i.second.second];
 			if (a == b) continue;
 			if (v[a].size() < v[b].size()) swap(a, b);
-			for (auto j : v[b]) {
-				id[j] = a;
-				v[a].push_back(j);
-			}
+			for (auto j : v[b]) id[j] = a, v[a].push_back(j);
 			val[a].push_back(i.first);
 			for (auto j : val[b]) val[a].push_back(j);
 			v[b].clear(), val[b].clear();
 		}
 		for (int i = 0; i < n; i++) pos[v[id[0]][i]] = i;
 		for (int i = n; i < 2*n-1; i++) seg[i] = val[id[0]][i-n];
-		seg[2*n-1] = INF;
 		for (int i = n-1; i > 0; i--) seg[i] = min(seg[2*i], seg[2*i+1]);
 	}
 	int query(int a, int b) {

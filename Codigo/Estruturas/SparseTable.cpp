d9fb0a85843068b725df8f0f3e7b495d02bd5801
brunomaletta <31869510+brunomaletta@users.noreply.github.com>
2020-01-07 19:55:34 -0300

    Update SparseTable.cpp

--- Codigo/Estruturas/SparseTable.cpp
+++ Codigo/Estruturas/SparseTable.cpp
@@ -12,7 +12,7 @@ int v[MAX];
 int m[MAX][MAX2]; // m[i][j] : minimo de v[i, i + 2^j - 1]
 
 void build() {
-	for (int i = 0; i < n; i++) m[i][0] = i;
+	for (int i = 0; i < n; i++) m[i][0] = v[i];
 	for (int j=1; 1 << j <= n; j++) for (int i=0; i+(1<<j) <= n; i++)
 		m[i][j] = min(m[i][j-1], m[i+(1<<(j-1))][j-1]);
 }

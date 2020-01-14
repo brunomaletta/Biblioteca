a20dc144452d8e2e22c9a349736d4590b0e8606d
brunomaletta <31869510+brunomaletta@users.noreply.github.com>
2020-01-14 11:29:51 -0300

    Update SparseTable.cpp

--- Codigo/Estruturas/SparseTable.cpp
+++ Codigo/Estruturas/SparseTable.cpp
@@ -7,17 +7,16 @@
 // build - O(n log(n))
 // query - O(1)
 
-int n;
-int v[MAX];
-int m[MAX][MAX2]; // m[i][j] : minimo de v[i, i + 2^j - 1]
-
-void build() {
-	for (int i = 0; i < n; i++) m[i][0] = v[i];
-	for (int j=1; 1 << j <= n; j++) for (int i=0; i+(1<<j) <= n; i++)
-		m[i][j] = min(m[i][j-1], m[i+(1<<(j-1))][j-1]);
-}
-
-int query(int a, int b) {
-	int j = __builtin_clz(0) - __builtin_clz(b-a+1) - 1;
-	return min(m[a][j], m[b-(1<<j)+1][j]);
+namespace sparse {
+	int m[MAX2][MAX], n;
+	void build(int n2, int* v) {
+		n = n2;
+		for (int i = 0; i < n; i++) m[0][i] = v[i];
+		for (int j = 1; (1<<j) <= n; j++) for (int i = 0; i+(1<<j) <= n; i++)
+			m[j][i] = min(m[j-1][i], m[j-1][i+(1<<(j-1))]);
+	}
+	int query(int a, int b) {
+		int j = __builtin_clz(0) - __builtin_clz(b-a+1) - 1;
+		return min(m[j][a], m[j][b-(1<<j)+1]);
+	}
 }

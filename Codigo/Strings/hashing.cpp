e6cfd7e43164ccfc34549afe1e60b03bc1007fb0
Rafael Grandsire <rafael.grandsire@gmail.com>
2020-02-10 20:48:38 -0300

    fix typo

--- Codigo/Strings/hashing.cpp
+++ Codigo/Strings/hashing.cpp
@@ -9,7 +9,7 @@
 //
 // Complexidades:
 // build - O(|s|)
-// get_hash - O(1)
+// operator() - O(1)
 
 mt19937 rng((int) chrono::steady_clock::now().time_since_epoch().count());
 
@@ -29,7 +29,7 @@ template<int MOD> struct str_hash {
 		h[0] = s[0];
 		for (int i = 1; i < n; i++) h[i] = (h[i-1]*P + s[i]) % MOD;
 	}
-	ll operator()(int i, int j){
+	ll operator()(int i, int j){//returns hash of substring s[i ... j]
 		if (!i) return h[j];
 		return (h[j] - h[i-1]*power[j-i+1] % MOD + MOD) % MOD;
 	}

417052a915cd226025e52e636e17e4ec6296bcea
Bruno Monteiro <malettabruno@gmail.com>
2020-02-25 13:37:39 -0300

    better lct

--- Codigo/Grafos/lct-aresta.cpp
+++ Codigo/Grafos/lct-aresta.cpp
@@ -11,14 +11,14 @@
 
 namespace lct {
 	struct node {
-		int p, path_p, ch[2];
+		int p, ch[2];
 		ll val, sub;
 		bool rev;
 		int sz, ar;
 		ll lazy;
 		node() {}
 		node(int v, int ar_) :
-		p(-1), path_p(-1), val(v), sub(v), rev(0), sz(ar_), ar(ar_), lazy(0) {
+		p(-1), val(v), sub(v), rev(0), sz(ar_), ar(ar_), lazy(0) {
 			ch[0] = ch[1] = -1;
 		}
 	};
@@ -29,7 +29,8 @@ namespace lct {
 
 	void prop(int x) {
 		if (t[x].lazy) {
-			t[x].val += t[x].lazy*t[x].ar, t[x].sub += t[x].lazy*t[x].sz;
+			if (t[x].ar) t[x].val += t[x].lazy;
+			t[x].sub += t[x].lazy*t[x].sz;
 			if (t[x].ch[0]+1) t[t[x].ch[0]].lazy += t[x].lazy;
 			if (t[x].ch[1]+1) t[t[x].ch[1]].lazy += t[x].lazy;
 		}
@@ -48,46 +49,32 @@ namespace lct {
 			t[x].sub += t[t[x].ch[i]].sub;
 		}
 	}
+	bool is_root(int x) {
+		return t[x].p == -1 or (t[t[x].p].ch[0] != x and t[t[x].p].ch[1] != x);
+	}
 	void rotate(int x) {
 		int p = t[x].p, pp = t[p].p;
-		if (pp+1) t[pp].ch[t[pp].ch[1] == p] = x;
+		if (!is_root(p)) t[pp].ch[t[pp].ch[1] == p] = x;
 		bool d = t[p].ch[0] == x;
 		t[p].ch[!d] = t[x].ch[d], t[x].ch[d] = p;
 		if (t[p].ch[!d]+1) t[t[p].ch[!d]].p = p;
 		t[x].p = pp, t[p].p = x;
-		t[x].path_p = t[p].path_p, t[p].path_p = -1;
 		update(p), update(x);
 	}
 	int splay(int x) {
-		int r = x;
-		vector<int> st = {r};
-		while (t[r].p+1) st.push_back(r = t[r].p);
-		while (st.size()) prop(st.back()), st.pop_back();
-		while (t[x].p+1) {
+		while (!is_root(x)) {
 			int p = t[x].p, pp = t[p].p;
-			if (pp == -1) return rotate(x), x;
-			if ((t[pp].ch[0] == p)^(t[p].ch[0] == x))
-				rotate(x), rotate(x);
-			else rotate(p), rotate(x);
+			if (!is_root(p)) prop(pp);
+			prop(p), prop(x);
+			if (!is_root(p)) rotate((t[pp].ch[0] == p)^(t[p].ch[0] == x) ? x : p);
+			rotate(x);
 		}
-		return x;
+		return prop(x), x;
 	}
 	int access(int v) {
-		splay(v);
-		if (t[v].ch[1]+1) {
-			t[t[v].ch[1]].path_p = v;
-			t[v].ch[1] = t[t[v].ch[1]].p = -1;
-			update(v);
-		}
-		int last = v;
-		for (int w; (w=t[v].path_p)+1; update(w), splay(v)) {
-			splay(last = w);
-			if (t[w].ch[1]+1) {
-				t[t[w].ch[1]].path_p = w;
-				t[t[w].ch[1]].p = -1;
-			}
-			t[w].ch[1] = v, t[v].p = w;
-		}
+		int last = -1;
+		for (int w = v; w+1; update(last = w), splay(v), w = t[v].p)
+			splay(w), t[w].ch[1] = (last == -1 ? -1 : v);
 		return last;
 	}
 	void make_tree(int v, int w=0, int ar=0) { t[v] = node(w, ar); }
@@ -111,7 +98,6 @@ namespace lct {
 	void link_(int v, int w) {
 		rootify(v), access(w);
 		t[w].ch[1] = v, t[v].p = w;
-		update(w);
 	}
 	void link(int v, int w, int x) { // v--w com peso x
 		int id = MAX + sz++;
@@ -122,7 +108,6 @@ namespace lct {
 	void cut_(int v, int w) {
 		rootify(w), access(v);
 		t[v].ch[0] = t[t[v].ch[0]].p = -1;
-		update(v);
 	}
 	void cut(int v, int w) {
 		int id = aresta[make_pair(v, w)];
--- Codigo/Grafos/lct-vertice.cpp
+++ Codigo/Grafos/lct-vertice.cpp
@@ -11,13 +11,13 @@
 
 namespace lct {
 	struct node {
-		int p, path_p, ch[2];
+		int p, ch[2];
 		ll val, sub;
 		bool rev;
 		int sz;
 		ll lazy;
 		node() {}
-		node(int v) : p(-1), path_p(-1), val(v), sub(v), rev(0), sz(1), lazy(0) {
+		node(int v) : p(-1), val(v), sub(v), rev(0), sz(1), lazy(0) {
 			ch[0] = ch[1] = -1;
 		}
 	};
@@ -45,46 +45,32 @@ namespace lct {
 			t[x].sub += t[t[x].ch[i]].sub;
 		}
 	}
+	bool is_root(int x) {
+		return t[x].p == -1 or (t[t[x].p].ch[0] != x and t[t[x].p].ch[1] != x);
+	}
 	void rotate(int x) {
 		int p = t[x].p, pp = t[p].p;
-		if (pp+1) t[pp].ch[t[pp].ch[1] == p] = x;
+		if (!is_root(p)) t[pp].ch[t[pp].ch[1] == p] = x;
 		bool d = t[p].ch[0] == x;
 		t[p].ch[!d] = t[x].ch[d], t[x].ch[d] = p;
 		if (t[p].ch[!d]+1) t[t[p].ch[!d]].p = p;
 		t[x].p = pp, t[p].p = x;
-		t[x].path_p = t[p].path_p, t[p].path_p = -1;
 		update(p), update(x);
 	}
 	int splay(int x) {
-		int r = x;
-		vector<int> st = {r};
-		while (t[r].p+1) st.push_back(r = t[r].p);
-		while (st.size()) prop(st.back()), st.pop_back();
-		while (t[x].p+1) {
+		while (!is_root(x)) {
 			int p = t[x].p, pp = t[p].p;
-			if (pp == -1) return rotate(x), x;
-			if ((t[pp].ch[0] == p)^(t[p].ch[0] == x))
-				rotate(x), rotate(x);
-			else rotate(p), rotate(x);
+			if (!is_root(p)) prop(pp);
+			prop(p), prop(x);
+			if (!is_root(p)) rotate((t[pp].ch[0] == p)^(t[p].ch[0] == x) ? x : p);
+			rotate(x);
 		}
-		return x;
+		return prop(x), x;
 	}
 	int access(int v) {
-		splay(v);
-		if (t[v].ch[1]+1) {
-			t[t[v].ch[1]].path_p = v;
-			t[v].ch[1] = t[t[v].ch[1]].p = -1;
-			update(v);
-		}
-		int last = v;
-		for (int w; (w=t[v].path_p)+1; update(w), splay(v)) {
-			splay(last = w);
-			if (t[w].ch[1]+1) {
-				t[t[w].ch[1]].path_p = w;
-				t[t[w].ch[1]].p = -1;
-			}
-			t[w].ch[1] = v, t[v].p = w;
-		}
+		int last = -1;
+		for (int w = v; w+1; update(last = w), splay(v), w = t[v].p)
+			splay(w), t[w].ch[1] = (last == -1 ? -1 : v);
 		return last;
 	}
 	void make_tree(int v, int w) { t[v] = node(w); }
@@ -108,16 +94,13 @@ namespace lct {
 	void link(int v, int w) {
 		rootify(v), access(w);
 		t[w].ch[1] = v, t[v].p = w;
-		update(w);
 	}
 	void cut(int v, int w) {
 		rootify(w), access(v);
 		t[v].ch[0] = t[t[v].ch[0]].p = -1;
-		update(v);
 	}
 	int lca(int v, int w) {
 		access(v);
 		return access(w);
 	}
 }
-

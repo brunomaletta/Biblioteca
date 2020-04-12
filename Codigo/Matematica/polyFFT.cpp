6a0d557d3bf7aae56a95db39150fb1d302f55fd0
Rafael Grandsire <rafael.grandsire@gmail.com>
2020-04-12 02:36:11 -0300

    Update polyFFT.cpp

--- Codigo/Matematica/polyFFT.cpp
+++ Codigo/Matematica/polyFFT.cpp
@@ -4,75 +4,16 @@
 //
 // Soma O(n) & Multiplicacao O(nlogn)
 
-const int MAX = 5e5;
-const int MAX2 = (1 << 20);//(1 << (ceil(log2(MAX)) + 1)) + 1
-
-const double PI = acos(-1);
-
-struct cplx{
-	double r, i;
-	cplx(double r_ = 0, double i_ = 0):r(r_), i(i_){}
-	const cplx operator+(const cplx &x) const{
-		return cplx(r + x.r, i + x.i);
-	}
-	const cplx operator-(const cplx &x) const{
-		return cplx(r - x.r, i - x.i);
-	}
-	const cplx operator*(double a) const {
-		return cplx(r*a, i*a);
-	}
-	const cplx operator/(double a)const {
-		return cplx(r/a, i/a);
-	}
-	const cplx operator*(const cplx x) const {
-		return cplx(r*x.r - i*x.i, r*x.i + i*x.r);
-	}
-};
-
-const cplx I(0, 1);
-cplx X[MAX2], Y[MAX2];
-int rev[MAX2];
-cplx roots[MAX2];
-
-cplx rt(int i, int n){
-	double alpha = (2*i*PI)/n;
-	return cplx(cos(alpha), sin(alpha));
-}
-
-void fft(cplx *a, bool f, int N){
-	for (int i = 0; i < N; i++)
-		if (i < rev[i])
-			swap(a[i], a[rev[i]]);
-	int l, r, m;
-	for (int n = 2; n <= N; n *= 2){
-		cplx root = (f ? rt(1, n) : rt(-1, n));
-		roots[0] = 1;
-		for (int i = 1; i < n/2; i++)
-			roots[i] = roots[i-1]*root;
-		for (int pos = 0; pos < N; pos += n){
-			l = pos+0, r = pos+n/2, m = 0;
-			while (m < n/2){
-				auto t = roots[m]*a[r];
-				a[r] = a[l] - t;
-				a[l] = a[l] + t;
-				l++; r++; m++;
-			}
-		}
-	}
-	if (f) for(int i = 0; i < N; i++) a[i] = a[i]/N;
-}
-
-
-//p(x) = at(i)*x^i
 template<typename T> struct poly : vector<T> {
 	poly(const vector<T> &coef):vector<T>(coef){}
-	poly(unsigned size):vector<T>(size){}
+	poly(unsigned size, T val = 0):vector<T>(size, val){}
 	poly(){}
 	T operator()(T x){
 		T ans = 0, curr_x(1);
 		for (auto c : *this) {
 			ans = ans+c*curr_x;
 			curr_x = curr_x*x;
+	
 		}
 		return ans;
 	}
@@ -106,34 +47,37 @@ template<typename T> struct poly : vector<T> {
 		const poly<T> &l = *this;
 		int ln = l.size(), rn = r.size();
 		int N = ln+rn+1;
-		int n = 1, log_n = 0;
-		while (n <= N) { n <<= 1; log_n++; }
+		int log_n = T::fft_len(N);
+		int n = 1 << log_n;
+		vector<int> rev(n);
+
 		for (int i = 0; i < n; ++i){
 			rev[i] = 0;
 			for (int j = 0; j < log_n; ++j)
 				if (i & (1<<j))
 					rev[i] |= 1 << (log_n-1-j);
 		}
+		if (N > n) throw logic_error("resulting poly to big");
+		vector<T> X(n), Y(n);
+
 		for (int i = 0; i < ln; i++) X[i] = l[i];
 		for (int i = ln; i < n; i++) X[i] = 0;
 		for (int i = 0; i < rn; i++) Y[i] = r[i];
 		for (int i = rn; i < n; i++) Y[i] = 0;
-		fft(X, false, n);//call dft if possible
-		fft(Y, false, n);
+
+		fft(X, false, log_n, n, rev);
+		fft(Y, false, log_n, n, rev);
 
 		for (int i = 0; i < n; i++)
 			Y[i] = X[i]*Y[i];
 
-		fft(Y, true, n);
+		fft(Y, true, log_n, n, rev);
 		poly<T> ans(N);
-		for (int i = 0; i < N; i++){
-			ans[i] = floor(Y[i].r + 0.25); //if T is integer
-			//ans[i] = Y[i].r; //if T is floating point
-		}
-			
-
-		while (!ans.empty() && ans.back() == 0)
-			ans.pop_back();
+		for (int i = 0; i < N; i++)
+			ans[i] = Y[i];
+		
+		//	while (!ans.empty() && ans.back() == 0)
+		//		ans.pop_back();
 		return ans;
 	}
 	pair<poly<T>, T> briot_ruffini(T r){//for p = Q(x - r) + R, returns (Q, R)
@@ -149,6 +93,8 @@ template<typename T> struct poly : vector<T> {
 		return {q, q[0]*r + l[0]};
 	}
 };
+
+
 template<typename T> ostream& operator<<(ostream &out, const poly<T> &p){
 	if (p.empty()) return out;
 	out << p.at(0);
@@ -162,7 +108,7 @@ int main(){ _
 	poly<int> p({-2, -1, 2, 1});
 	poly<int> q({1, 1, 1});
 	poly<int> sum = p+q;
-	poly<int> mult = p*q;
+	poly<int> mult = p*q;//int doesnt support mult, type should be cplx or mod_int<P> 
 	cout << "p: " << p << endl;
 	cout << "q: " << q << endl;
 	cout << "pq: " << mult << endl;

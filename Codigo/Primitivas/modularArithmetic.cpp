3548b655e0d828f1290ee84548653f5e0cc1de87
Bruno Monteiro <malettabruno@gmail.com>
2020-05-03 11:05:54 -0300

    poxa rafael

--- Codigo/Primitivas/modularArithmetic.cpp
+++ Codigo/Primitivas/modularArithmetic.cpp
@@ -2,73 +2,72 @@
 //
 // O mod tem q ser primo
 
-template<int p> struct mod_int{
-	ll pow(ll b, ll e){
+template<int p> struct mod_int {
+	ll pow(ll b, ll e) {
 		if (e == 0) return 1;
-		if (e == 1) return b;
-		ll r = pow(b, e/2);
-		r = (r*r)%p;
-		if (e%2 == 1)
-			r = (r*b)%p;
+		ll r = pow(b*b%p, e/2);
+		if (e%2 == 1) r = (r*b)%p;
 		return r;
 	}
-	ll inv(ll b){ return pow(b, p-2); }
+	ll inv(ll b) { return pow(b, p-2); }
 
 	using m = mod_int;
 	int v;
-	mod_int(){}
-	mod_int(ll v_):v(v_){
-		if (v >= p || v <= -p)
-			v %= p;
-		if (v < 0) v += p;
+	mod_int() {}
+	mod_int(ll v_) {
+		if (v_ >= p || v_ <= -p) v_ %= p;
+		if (v_ < 0) v_ += p;
+		v = v_;
 	}
-	m& operator+=(const m &a){
+	m& operator+=(const m &a) {
 		v += a.v;
 		if (v >= p) v -= p;
 		return *this;
 	}
-	m& operator-=(const m &a){
+	m& operator-=(const m &a) {
 		v -= a.v;
 		if (v < 0) v += p;
 		return *this;
 	}
-	m& operator*=(const m &a){
+	m& operator*=(const m &a) {
 		v = (v*ll(a.v))%p;
 		return *this;
 	}
-	m& operator/=(const m &a){
+	m& operator/=(const m &a) {
 		v = (v*inv(a.v))%p;
 		return *this;
 	}
 	m operator-(){ return m(-v); }
-	m& operator^=(ll e){
+	m& operator^=(ll e) {
 		if (e < 0){
 			v = inv(v);
 			e = -e;
 		}
-		v = pow(v, e);
+		v = pow(v, e%(p-1));
 		return *this;
 	}
-	bool operator==(const m &a){ return v == a.v; }
-	bool operator!=(const m &a){ return v != a.v; }
-	friend ostream &operator<<(ostream &out, m a){
-		return out << a.v;
+	bool operator==(const m &a) { return v == a.v; }
+	bool operator!=(const m &a) { return v != a.v; }
+	friend istream &operator>>(istream &in, m& a) {
+		ll val; in >> val;
+		a = m(val);
+		return in;
 	}
-	friend m operator+(m a, m b){ return a+=b; }
-	friend m operator-(m a, m b){ return a-=b; }
-	friend m operator*(m a, m b){ return a*=b; }
-	friend m operator/(m a, m b){ return a/=b; }
-	friend m operator^(m a, ll e){ return a^=e; }
+	friend ostream &operator<<(ostream &out, m a) {
+	friend m operator+(m a, m b) { return a+=b; }
+	friend m operator-(m a, m b) { return a-=b; }
+	friend m operator*(m a, m b) { return a*=b; }
+	friend m operator/(m a, m b) { return a/=b; }
+	friend m operator^(m a, ll e) { return a^=e; }
 
 	static int fft_len(int n = -1){
-		//max k such that 2^k | p-1
-		if (p == 998244353)
-			return 20;
+		// max k such that 2^k | p-1
+		if (p == 998244353) return 20;
 		throw logic_error("find an order");
 		return -1;
 	}
 	static m rt(bool f, int n, int N){
-		//an element of order fft_len
+		// an element of order fft_len
 		if (p == 998244353){
 			m r(695449733);
 			if (f) r = r^(-1);
@@ -76,7 +75,7 @@ template<int p> struct mod_int{
 		}
 
 		throw logic_error("find a root");
-		return -1;//return x so that x^(2^k) != x*x^(2^k) = 1
+		return -1; // return x so that x^(2^k) != x*x^(2^k) = 1
 	}
 
 };

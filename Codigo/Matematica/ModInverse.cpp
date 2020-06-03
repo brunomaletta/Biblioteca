391989eee1386024cfb2afd6bda02b6e790337cc
Bruno Monteiro <malettabruno@gmail.com>
2020-06-02 22:54:57 -0300

    Add string hashing modulo 2^61-1

--- Codigo/Matematica/ModInverse.cpp
+++ Codigo/Matematica/ModInverse.cpp
@@ -4,6 +4,11 @@
 // Se b eh primo, basta fazer
 // a^(b-2)
 
-long long inv(long long a, long long b){
-	return 1<a ? b - inv(b%a,a)*b/a : 1;
+ll inv(ll a, ll b) {
+	return a > 1 ? b - inv(b%a, a)*b/a : 1;
 }
+
+// computa o inverso modular de 1..MAX-1 modulo um primo
+ll inv[MAX]:
+inv[1] = 1;
+for (int i = 2; i < MAX; i++) inv[i] = MOD - MOD/i*inv[MOD%i]%MOD;
--- Codigo/Strings/hashing.cpp
+++ Codigo/Strings/hashing.cpp
@@ -1,6 +1,5 @@
 // String hashing
 //
-// String deve ter valores [1, x]
 // Para evitar colisao: testar mais de um
 // mod; so comparar strings do mesmo tamanho
 // ex : str_hash<1e9+7> h(s);
@@ -12,7 +11,7 @@
 
 mt19937 rng((int) chrono::steady_clock::now().time_since_epoch().count());
 
-int uniform(int l, int r){
+int uniform(int l, int r) {
 	uniform_int_distribution<int> uid(l, r);
 	return uid(rng);
 }
@@ -22,15 +21,16 @@ template<int MOD> struct str_hash {
 	int n;
 	string s;
 	vector<ll> h, power;
-	str_hash(string s_): n(s_.size()), s(s_), h(n), power(n){
+	str_hash(string s_) : n(s_.size()), s(s_), h(n), power(n) {
 		power[0] = 1;
 		for (int i = 1; i < n; i++) power[i] = power[i-1]*P % MOD;
 		h[0] = s[0];
 		for (int i = 1; i < n; i++) h[i] = (h[i-1]*P + s[i]) % MOD;
 	}
-	ll operator()(int i, int j){ // retorna hash da substring s[i..j]
+	ll operator()(int i, int j) { // retorna hash da substring s[i..j]
 		if (!i) return h[j];
-		return (h[j] - h[i-1]*power[j-i+1] % MOD + MOD) % MOD;
+		ll ret = h[j] - h[i-1]*power[j-i+1] % MOD;
+		return ret < 0 ? ret+MOD : ret;
 	}
 };
 template<int MOD> int str_hash<MOD>::P = uniform(27, MOD-1);

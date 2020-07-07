d0f5edc0c2f2feea8dd73b2c513421da66fc1a38
Bruno Monteiro <malettabruno@gmail.com>
2020-07-07 12:29:24 -0300

    Add min vertex cover

--- Codigo/Grafos/kuhn.cpp
+++ Codigo/Grafos/kuhn.cpp
@@ -6,6 +6,8 @@
 // da particao A, e o cara j da particao B
 // (entao i < n, j < m)
 // Para recuperar o matching, basta olhar 'ma' e 'mb'
+// cover() retorna o min vertex cover como um par de
+// {caras da particao A, caras da particao B}
 //
 // O(n m)
 // Na pratica, parece rodar tao rapido quanto o Dinic
@@ -18,13 +20,14 @@ struct kuhn {
 	vector<int> vis, ma, mb;
 
 	kuhn(int n_, int m_) : n(n_), m(m_), g(n),
-		vis(m), ma(n, -1), mb(m, -1) {}
+		vis(n+m), ma(n, -1), mb(m, -1) {}
 
 	void add(int a, int b) { g[a].pb(b); }
 
-	bool dfs(int i){
-		for (int j : g[i]) if (!vis[j]) {
-			vis[j] = 1;
+	bool dfs(int i) {
+		vis[i] = 1;
+		for (int j : g[i]) if (!vis[n+j]) {
+			vis[n+j] = 1;
 			if (mb[j] == -1 or dfs(mb[j])) {
 				ma[i] = j, mb[j] = i;
 				return true;
@@ -36,11 +39,21 @@ struct kuhn {
 		int ret = 0, aum = 1;
 		for (auto& i : g) shuffle(i.begin(), i.end(), rng);
 		while (aum) {
-			for (int j = 0; j < m; j++) vis[j] = 0;
+			for (int j = 0; j < m; j++) vis[n+j] = 0;
 			aum = 0;
 			for (int i = 0; i < n; i++)
 				if (ma[i] == -1 and dfs(i)) ret++, aum = 1;
 		}
 		return ret;
+	}
+	pair<vector<int>, vector<int>> cover() {
+		int M = matching();
+		for (int i = 0; i < n+m; i++) vis[i] = 0;
+		for (int i = 0; i < n; i++) if (ma[i] == -1) assert(!dfs(i));
+		vector<int> ca, cb;
+		for (int i = 0; i < n; i++) if (!vis[i]) ca.push_back(i);
+		for (int i = 0; i < m; i++) if (vis[n+i]) cb.push_back(i);
+		//assert(ca.size() + cb.size() == M);
+		return {ca, cb};
 	}
 };

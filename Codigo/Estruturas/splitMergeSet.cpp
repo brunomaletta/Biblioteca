a1e0edd04ed8f9fc8b3d804d8492e6f18dee1dba
Bruno Monteiro <malettabruno@gmail.com>
2020-07-27 09:27:25 -0300

    Codigo um pouco menor pro split-merge set

--- Codigo/Estruturas/splitMergeSet.cpp
+++ Codigo/Estruturas/splitMergeSet.cpp
@@ -1,13 +1,14 @@
 // Split-Merge Set
 //
-// Representa um conjunto de inteiros
+// Representa um conjunto de inteiros nao negativos
 // Todas as operacoes custam O(log(N)),
 // em que N = maior elemento do set,
 // exceto o merge, que custa O(log(N)) amortizado
-// Usa O(n log(N)) de memoria, sendo 'n' o numero de
-// elementos distintos no set
+// Usa O(min(N, n log(N))) de memoria, sendo 'n' o
+// numero de elementos distintos no set
 
 template<typename T, bool MULTI=false, typename SIZE_T=int> struct sms {
+	#define SMS sms<T, MULTI, SIZE_T>
 	struct node {
 		node *l, *r;
 		SIZE_T cnt;
@@ -23,9 +24,7 @@ template<typename T, bool MULTI=false, typename SIZE_T=int> struct sms {
 	T N;
 
 	sms() : root(NULL), N(0) {}
-	sms(T v) : sms() {
-		while (v >= N) N = 2*N+1;
-	}
+	sms(T v) : sms() { while (v >= N) N = 2*N+1; }
 	sms(const sms& t) : root(NULL), N(t.N) {
 		for (SIZE_T i = 0; i < t.size(); i++) {
 			T at = t[i];
@@ -34,9 +33,7 @@ template<typename T, bool MULTI=false, typename SIZE_T=int> struct sms {
 			i += qt-1;
 		}
 	}
-	sms(initializer_list<T> v) : sms() {
-		for (T i : v) insert(i);
-	}
+	sms(initializer_list<T> v) : sms() { for (T i : v) insert(i); }
 	~sms() {
 		vector<node*> q = {root};
 		while (q.size()) {
@@ -47,15 +44,22 @@ template<typename T, bool MULTI=false, typename SIZE_T=int> struct sms {
 		}
 	}
 
-	friend void swap(sms<T, MULTI, SIZE_T>& a, sms<T, MULTI, SIZE_T>& b) {
+	friend void swap(SMS& a, SMS& b) {
 		swap(a.root, b.root), swap(a.N, b.N);
 	}
-
 	SIZE_T size() const { return root ? root->cnt : 0; }
 	SIZE_T count(node* x) const { return x ? x->cnt : 0; }
 	void clear() {
 		sms<T, MULTI, SIZE_T> tmp;
 		swap(*this, tmp);
+	}
+	void expand(T v) {
+		for (; N < v; N = 2*N+1) if (root) {
+			node* nroot = new node();
+			nroot->l = root;
+			root = nroot;
+			root->update();
+		}
 	}
 
 	node* insert(node* at, T idx, SIZE_T qt, T l, T r) {
@@ -73,31 +77,20 @@ template<typename T, bool MULTI=false, typename SIZE_T=int> struct sms {
 	void insert(T v, SIZE_T qt=1) { // insere 'qt' ocorrencias de 'v'
 		if (qt <= 0) return erase(v, -qt);
 		assert(v >= 0);
-		while (v > N) {
-			if (root) {
-				node* nroot = new node();
-				nroot->l = root;
-				root = nroot;
-				root->update();
-			}
-			N = 2*N+1;
-		}
+		expand(v);
 		root = insert(root, v, qt, 0, N);
 	}
 
 	node* erase(node* at, T idx, SIZE_T qt, T l, T r) {
 		if (!at) return at;
-		if (l == r) at->cnt = qt == -1 ? 0 : max((SIZE_T)0, at->cnt - qt);
+		if (l == r) at->cnt = at->cnt < qt ? 0 : at->cnt - qt;
 		else {
 			T m = l + (r-l)/2;
 			if (idx <= m) at->l = erase(at->l, idx, qt, l, m);
 			else at->r = erase(at->r, idx, qt, m+1, r);
 			at->update();
 		}
-		if (!at->cnt) {
-			delete at;
-			return NULL;
-		}
+		if (!at->cnt) delete at, at = NULL;
 		return at;
 	}
 	void erase(T v, SIZE_T qt=1) { // remove 'qt' ocorrencias de 'v'
@@ -107,11 +100,12 @@ template<typename T, bool MULTI=false, typename SIZE_T=int> struct sms {
 	}
 	void erase_all(T v) { // remove todos os 'v'
 		if (v < 0 or v > N) return;
-		root = erase(root, v, -1, 0, N);
+		root = erase(root, v, numeric_limits<SIZE_T>::max(), 0, N);
 	}
 
 	void shrink() { // diminui o N em funcao do maior elemento no set
-		while (root and !root->r) {
+		if (!root) return void(N = 0);
+		while (!root->r) {
 			node* at = root;
 			root = root->l, N /= 2;
 			delete at;
@@ -124,16 +118,8 @@ template<typename T, bool MULTI=false, typename SIZE_T=int> struct sms {
 		T m = l + (r-l)/2;
 		return count(at->l, a, b, l, m) + count(at->r, a, b, m+1, r);
 	}
-	SIZE_T count(T v) const { // quantas ocorrencias de 'v' tem no set
-		if (v < 0 or v > N) return 0;
-		return count(root, v, v, 0, N);
-	}
-	SIZE_T order_of_key(T v) { // quantos elementos < 'v' tem no set
-		if (v <= 0) return 0;
-		if (v > N) return size();
-		return count(root, 0, v-1, 0, N);
-	}
-	// indice do primeiro elemento >= 'v' (ou size() se nao tem)
+	SIZE_T count(T v) const { return count(root, v, v, 0, N); }
+	SIZE_T order_of_key(T v) { return count(root, 0, v-1, 0, N); }
 	SIZE_T lower_bound(T v) { return order_of_key(v); }
 
 	const T operator [](SIZE_T i) const { // i-esimo menor elemento
@@ -162,17 +148,9 @@ template<typename T, bool MULTI=false, typename SIZE_T=int> struct sms {
 		l->update(), delete r;
 		return l;
 	}
-	void merge(sms<T, MULTI, SIZE_T>& s) { // mergeia dos sets
+	void merge(SMS& s) { // mergeia dois sets
 		if (N > s.N) swap(*this, s);
-		while (N < s.N) {
-			if (root) {
-				node* nroot = new node();
-				nroot->l = root;
-				root = nroot;
-				root->update();
-			}
-			N = 2*N+1;
-		}
+		expand(s.N);
 		root = merge(root, s.root);
 		s.root = NULL;
 	}
@@ -180,28 +158,24 @@ template<typename T, bool MULTI=false, typename SIZE_T=int> struct sms {
 	node* split(node*& x, SIZE_T k) {
 		if (k <= 0 or !x) return NULL;
 		node* ret = new node();
-		if (!x->l and !x->r) { // folha
-			x->cnt -= k;
-			ret->cnt += k;
-			if (!x->cnt) delete x, x = NULL;
-			return ret;
-		}
-		int l = count(x->l);
-		if (k <= l) ret->l = split(x->l, k);
+		if (!x->l and !x->r) x->cnt -= k, ret->cnt += k;
 		else {
-			swap(x->l, ret->l);
-			ret->r = split(x->r, k - l);
+			if (k <= count(x->l)) ret->l = split(x->l, k);
+			else {
+				ret->r = split(x->r, k - count(x->l));
+				swap(x->l, ret->l);
+			}
+			ret->update(), x->update();
 		}
-		ret->update(), x->update();
 		if (!x->cnt) delete x, x = NULL;
 		return ret;
 	}
-	void split(SIZE_T k, sms<T, MULTI, SIZE_T>& s) { // os 'k' menores
+	void split(SIZE_T k, SMS& s) { // pega os 'k' menores
 		s.clear();
 		s.root = split(root, min(k, size()));
 		s.N = N;
 	}
-	void split_val(T k, sms<T, MULTI, SIZE_T>& s) { // os menores que 'k'
+	void split_val(T k, SMS& s) { // pega os menores que 'k'
 		split(order_of_key(k), s);
 	}
 };

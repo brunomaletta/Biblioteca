5b9a6156e781ed1a2b9190210da3346d14ae627e
Bernardo Amorim <bernardotamorim@gmail.com>
2020-08-13 08:58:45 -0300

    Add fix e comentario lis2

--- Codigo/Problemas/lis2.cpp
+++ Codigo/Problemas/lis2.cpp
@@ -1,11 +1,14 @@
-// LIS2
+// LIS2 - Longest Increasing Subsequence
 //
-// O(n log(n))
+// Calcula o tamanho da LIS
+//
+// O(n.log(n))
 
 template<typename T> int lis(vector<T> &v){
 	vector<T> ans;
 	for (T t : v){
-		auto it = upper_bound(ans.begin(), ans.end(), t);
+		// Para non-decreasing use upper_bound()
+		auto it = lower_bound(ans.begin(), ans.end(), t);
 		if (it == ans.end()) ans.push_back(t);
 		else *it = t;
 	}

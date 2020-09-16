9218e18f1561a7679f1ddd32138b163461e373ed
Bruno Monteiro <malettabruno@gmail.com>
2020-09-16 00:35:24 -0300

    oops

--- Codigo/Grafos/directedMST.cpp
+++ Codigo/Grafos/directedMST.cpp
@@ -6,28 +6,6 @@
 //
 // O(m log(n))
 
-int n;
-vector<pair<int, pair<int, int> > > ar; // vetor de arestas
-int v[MAX];
-
-// Union-Find em O(log(n))
-void build();
-int find(int k);
-void une(int a, int b);
-
-void kruskal() {
-	build();
-
-	sort(ar.begin(), ar.end());
-	for (int i = 0; i < (int) ar.size(); i++) {
-		int a = ar[i].s.f, b = ar[i].s.s;
-		if (find(a) != find(b)) {
-			une(a, b);
-			// aresta faz parte da AGM
-		}
-	}
-}
-
 struct node {
 	pair<ll, int> val;
 	ll lazy;

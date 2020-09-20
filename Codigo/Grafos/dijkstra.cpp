4757117182851a8a3115311de2cc52b4a315cf4b
Bernardo Amorim <bernardotamorim@gmail.com>
2020-09-19 23:31:47 -0300

    Update Dijkstra

--- Codigo/Grafos/dijkstra.cpp
+++ Codigo/Grafos/dijkstra.cpp
@@ -1,34 +1,30 @@
 // Dijkstra
 //
-// encontra menor distancia de a
+// encontra menor distancia de x
 // para todos os vertices
 // se ao final do algoritmo d[i] = INF,
-// entao a nao alcanca i
+// entao x nao alcanca i
 //
 // O(m log(n))
 
-int n;
-vector<int> g[MAX];
-vector<int> w[MAX]; // peso das arestas
 int d[MAX];
+vector<vector<pair<int,int>>> g(MAX); // {vizinho, custo}
 
-void dijsktra(int a) {
-	for (int i = 0; i < n; i++) d[i] = INF;
-	d[a] = 0;
-	priority_queue<pair<int, int> > Q;
-	Q.push(make_pair(0, a));
-
-	while (Q.size()) {
-		int u = Q.top().second, dist = -Q.top().first;
-		Q.pop();
-		if (dist > d[u]) continue;
-
-		for (int i = 0; i < (int) g[u].size(); i++) {
-			int v = g[u][i];
-			if (d[v] > d[u] + w[u][i]) {
-				d[v] = d[u] + w[u][i];
-				Q.push(make_pair(-d[v], v));
-			}
+int n;
+ 
+void dijkstra(int x) {
+	for(int i=0; i < n; i++) d[i] = INF;
+	d[x] = 0;
+	priority_queue<pair<int,int>> pq;
+	pq.push({0,x});
+ 
+	while(q.size()) {
+		auto [dist,u] = pq.top(); pq.pop();
+		if(dist > d[u]) continue;
+ 
+		for(auto p : g[u]) if(d[p.f] > d[u] + p.s) {
+			d[p.f] = d[u] + p.s;
+			pq.push({-d[p.f], p.f});
 		}
 	}
 }

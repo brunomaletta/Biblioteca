a905f4395d2e6ab6e0e77ebfb9c4be921dfaae38
Bernardo Amorim <bernardotamorim@gmail.com>
2020-10-02 14:29:29 -0300

    Add topological sort

---
+++ Codigo/Grafos/topo_sort.cpp
@@ -0,0 +1,27 @@
+// Topological Sort
+//
+// Retorna uma ordenação topológica de g
+// Se g não for DAG retorna um vetor vazio
+//
+// O(n + m)
+
+vector<int> g[MAX];
+
+vector<int> topo_sort(int n) {
+	vector<int> ret(n,-1), vis(n,0);
+	
+	int pos = n-1, dag = 1;
+	function<void(int)> dfs = [&] (int v) {
+		vis[v] = 1;
+		for(auto u : g[v]) {
+			if(vis[u] == 1) dag = 0;
+			else if(!vis[u]) dfs(u);
+		}
+		ret[pos--] = v, vis[v] = 2;
+	};
+
+	for(int i=0; i<n; i++) if(!vis[i]) dfs(i);
+
+	if(!dag) ret.clear();	
+	return ret;
+}

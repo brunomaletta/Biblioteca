481b5bddb86988cff856923d1b2cd3b6e1c84463
Bernardo Amorim <bernardotamorim@gmail.com>
2020-10-02 14:28:10 -0300

    Better Kruskal

--- Codigo/Grafos/kruskal.cpp
+++ Codigo/Grafos/kruskal.cpp
@@ -1,27 +1,29 @@
 // Kruskal
 //
-// Gera AGM a partir do vetor de arestas
+// Gera e retorna uma AGM e seu custo total a partir do vetor de arestas (edg)
+// do grafo
 //
-// O(m log(n))
+// O(m log(m) + m a(m))
 
-int n;
-vector<pair<int, pair<int, int> > > ar; // vetor de arestas
-int v[MAX];
+typedef tuple<int,int,int> t3;
 
-// Union-Find em O(log(n))
-void build();
-int find(int k);
+vector<t3> edg; // {peso,[x,y]}
+
+// DSU em O(a(n))
+void dsu_build();
+int find(int a);
 void une(int a, int b);
 
-void kruskal() {
-	build();
-
-	sort(ar.begin(), ar.end());
-	for (int i = 0; i < (int) ar.size(); i++) {
-		int a = ar[i].s.f, b = ar[i].s.s;
-		if (find(a) != find(b)) {
-			une(a, b);
-			// aresta faz parte da AGM
-		}
+pair<ll,vector<t3>> kruskal(int n) {
+	dsu_build(n);
+	sort(edg.begin(), edg.end());
+	
+	ll cost = 0;
+	vector<t3> mst;
+	for(auto [w,x,y] : edg) if(find(x) != find(y)) {
+		mst.push_back({w,x,y});
+		cost += w;
+		unite(x,y);
 	}
+	return {cost,mst};
 }

8de14efe19389a10f68198e9f102330ae3537f43
Bruno Monteiro <malettabruno@gmail.com>
2020-10-28 12:48:55 -0300

    Better inv count

--- Codigo/Problemas/InversionCount.cpp
+++ Codigo/Problemas/InversionCount.cpp
@@ -1,34 +1,31 @@
 // Inversion Count
 //
+// Computa o numero de inversoes para transformar
+// l em r (se nao tem como, retorna -1)
+//
 // O(n log(n))
 
-int n;
-int v[MAX];
-
-// bit de soma
-void poe(int p);
-int query(int p);
-
-// converte valores do array pra
-// numeros de 1 a n
-void conv() {
-	vector<int> a;
-	for (int i = 0; i < n; i++) a.push_back(v[i]);
-
-	sort(a.begin(), a.end());
-
-	for (int i = 0; i < n; i++)
-		v[i] = 1 + (lower_bound(a.begin(), a.end(), v[i]) - a.begin());
-}
-
-long long inv() {
-	conv();
-	build();
-
-	long long ret = 0;
-	for (int i = n - 1; i >= 0; i--) {
-		ret += query(v[i] - 1);
-		poe(v[i]);
+template<typename T> ll inv_count(vector<T> l, vector<T> r = {}) {
+	if (!r.size()) {
+		r = l;
+		sort(r.begin(), r.end());
 	}
-	return ret;
+	int n = l.size();
+	vector<int> v(n), bit(n);
+	vector<pair<T, int>> w;
+	for (int i = 0; i < n; i++) w.push_back({r[i], i+1});
+	sort(w.begin(), w.end());
+	for (int i = 0; i < n; i++) {
+		auto it = lower_bound(w.begin(), w.end(), make_pair(l[i], 0));
+		if (it == w.end() or it->first != l[i]) return -1; // nao da
+		v[i] = it->second;
+		it->second = -1;
+	}
+
+	ll ans = 0;
+	for (int i = n-1; i >= 0; i--) {
+		for (int j = v[i]-1; j; j -= j&-j) ans += bit[j];
+		for (int j = v[i]; j < n; j += j&-j) bit[j]++;
+	}
+	return ans;
 }

a7b81e9c38dd484b87bca90c03c1e15f260d279c
Bruno Monteiro <malettabruno@gmail.com>
2020-11-24 19:51:12 -0300

    MergeSortTree mais show ainda

--- Codigo/Estruturas/mergeSortTree.cpp
+++ Codigo/Estruturas/mergeSortTree.cpp
@@ -1,25 +1,31 @@
 // MergeSort Tree
 //
 // Se for construida sobre um array:
-//		query(i, j, a, b) retorna quantos
+//		count(i, j, a, b) retorna quantos
 //		elementos de v[i..j] pertencem a [a, b]
 //		report(i, j, a, b) retorna os indices dos
 //		elementos de v[i..j] que pertencem a [a, b]
 //		retorna o vetor ordenado
 // Se for construida sobre pontos (x, y):
-//		query(x1, x2, y1, x2) retorna quantos pontos
+//		count(x1, x2, y1, x2) retorna quantos pontos
 //		pertencem ao retangulo (x1, y1), (x2, y2)
 //		report(x1, x2, y1, y2) retorna os indices dos pontos que
 //		pertencem ao retangulo (x1, y1), (x2, y2)
 //		retorna os pontos ordenados lexicograficamente
 //		(assume x1 <= x2, y1 <= y2)
 //
+// kth(y1, y2, k) retorna o indice do ponto com k-esimo menor
+// x dentre os pontos que possuem y em [y1, y2] (0 based)
+// Se quiser usar para achar k-esimo valor em range, construir
+// com ms_tree t(v, true), e chamar kth(l, r, k)
+//
 // Usa O(n log(n)) de memoria
 //
 // Complexidades:
 // construir - O(n log(n))
-// query - O(log(n))
+// count - O(log(n))
 // report - O(log(n) + k) para k indices retornados
+// kth - O(log(n))
 
 template <typename T = int> struct ms_tree {
 	vector<tuple<T, T, int>> v;
@@ -33,9 +39,10 @@ template <typename T = int> struct ms_tree {
 		build(1, 0, n-1);
 		for (int i = 0; i < n; i++) vy[i] = get<0>(t[1][i+1]);
 	}
-	ms_tree(vector<T>& vv) {
+	ms_tree(vector<T>& vv, bool inv = false) { // inv: inverte indice e valor
 		vector<pair<T, T>> v2;
-		for (int i = 0; i < vv.size(); i++) v2.push_back({i, vv[i]});
+		for (int i = 0; i < vv.size(); i++)
+			inv ? v2.push_back({vv[i], i}) : v2.push_back({i, vv[i]});
 		*this = ms_tree(v2);
 	}
 	void build(int p, int l, int r) {
@@ -60,7 +67,7 @@ template <typename T = int> struct ms_tree {
 	int get_l(T y) { return lower_bound(vy.begin(), vy.end(), y) - vy.begin(); }
 	int get_r(T y) { return upper_bound(vy.begin(), vy.end(), y) - vy.begin(); }
 
-	int query(T x1, T x2, T y1, T y2) {
+	int count(T x1, T x2, T y1, T y2) {
 		function<int(int, int, int)> dfs = [&](int p, int l, int r) {
 			if (l == r or x2 < get<0>(t[p][0]) or get<1>(t[p][0]) < x1) return 0;
 			if (x1 <= get<0>(t[p][0]) and get<1>(t[p][0]) <= x2) return r-l;
@@ -82,5 +89,19 @@ template <typename T = int> struct ms_tree {
 		};
 		dfs(1, get_l(y1), get_r(y2));
 		return ret;
+	}
+	int kth(T y1, T y2, int k) {
+		function<int(int, int, int)> dfs = [&](int p, int l, int r) {
+			if (k >= r-l) {
+				k -= r-l;
+				return -1;
+			}
+			if (r-l == 1) return get<1>(t[p][l+1]);
+			int nl = get<2>(t[p][l]), nr = get<2>(t[p][r]);
+			int left = dfs(2*p, nl, nr);
+			if (left != -1) return left;
+			return dfs(2*p+1, l-nl, r-nr);
+		};
+		return dfs(1, get_l(y1), get_r(y2));
 	}
 };

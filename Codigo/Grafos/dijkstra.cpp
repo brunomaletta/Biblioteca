849bed0d401524838e3219537ea8092259f186e9
emanuel juliano morais silva <46662636+emanueljuliano@users.noreply.github.com>
2021-01-29 19:39:43 -0300

    Update dijkstra.cpp

--- Codigo/Grafos/dijkstra.cpp
+++ Codigo/Grafos/dijkstra.cpp
@@ -7,15 +7,15 @@
 //
 // O(m log(n))
 
-int d[MAX];
+ll d[MAX];
 vector<pair<int,int>> g[MAX]; // {vizinho, peso}
 
 int n;
  
 void dijkstra(int x) {
-	for(int i=0; i < n; i++) d[i] = INF;
+	for(int i=0; i < n; i++) d[i] = LINF;
 	d[x] = 0;
-	priority_queue<pair<int,int>> pq;
+	priority_queue<pair<ll,int>> pq;
 	pq.push({0,x});
  
 	while(pq.size()) {

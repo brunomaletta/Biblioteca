df1cc0372e58c3b7fc889e911d84cb7055d06bf8
Bruno Monteiro <malettabruno@gmail.com>
2021-03-06 13:44:50 -0300

    Template menor

--- Codigo/Extra/template.cpp
+++ Codigo/Extra/template.cpp
@@ -4,12 +4,8 @@ using namespace std;
 
 #define _ ios_base::sync_with_stdio(0);cin.tie(0);
 #define endl '\n'
-#define f first
-#define s second
-#define pb push_back
 
 typedef long long ll;
-typedef pair<int, int> ii;
 
 const int INF = 0x3f3f3f3f;
 const ll LINF = 0x3f3f3f3f3f3f3f3fll;

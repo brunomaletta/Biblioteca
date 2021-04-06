e565ada74a8a807fe8413317ddc224ec2494cd4e
Bruno Monteiro <malettabruno@gmail.com>
2021-04-06 19:26:53 -0300

    Add block-cut tree

---
+++ Codigo/Grafos/blockCutTree.cpp
@@ -0,0 +1,54 @@
+// Block-Cut Tree
+//
+// Cria a block-cut tree, uma arvore com os blocos
+// e os pontos de articulacao
+// Blocos sao componentes 2-vertice-conexos maximais
+// art[i] responde se i eh ponto de articulacao
+// Funciona pra grafo nao conexo, e ja limpa tudo
+//
+// O(n+m)
+
+vector<int> g[MAX];
+stack<int> s;
+int id[MAX], art[MAX], pos[MAX];
+vector<vector<int>> blocks, tree;
+
+int dfs(int i, int &t, int p = -1) {
+	int lo = id[i] = t++;
+	s.push(i);
+	for (int j : g[i]) if(j != p) {
+		if (id[j] == -1) {
+			int val = dfs(j, t, i);
+			lo = min(lo, val);
+
+			if (val >= id[i]) {
+				art[i]++;
+				blocks.emplace_back(1, i);
+				while (blocks.back().back() != j)
+					blocks.back().push_back(s.top()), s.pop();
+			}
+			// if (val > id[i]) aresta i-j eh ponte
+		}
+		else lo = min(lo, id[j]);
+	}
+	if (p == -1 and art[i]) art[i]--;
+	return lo;
+}
+
+void build(int n) {
+	for (int i = 0; i < n; i++) id[i] = -1, art[i] = 0;
+	blocks.clear(), tree.clear();
+	while (s.size()) s.pop();
+	int t = 0;
+
+	for (int i = 0; i < n; i++) if (id[i] == -1) dfs(i, t, -1);
+
+	tree.resize(blocks.size()); // no maximo 2*n
+	for (int i = 0; i < n; i++) if (art[i])
+		pos[i] = tree.size(), tree.emplace_back();
+
+	for (int i = 0; i < blocks.size(); i++) for (int j : blocks[i]) {
+		if (!art[j]) pos[j] = i;
+		else tree[i].push_back(pos[j]), tree[pos[j]].push_back(i);
+	}
+}
--- Codigo/Grafos/tarjan.cpp
+++ Codigo/Grafos/tarjan.cpp
@@ -5,20 +5,21 @@
 vector<int> g[MAX];
 stack<int> s;
 int vis[MAX], comp[MAX];
-int id[MAX], p;
+int id[MAX];
 
-// se quiser comprimir ciclo em grafo nao direcionado,
+// se quiser comprimir ciclo ou achar ponte em grafo nao direcionado,
 // colocar um if na dfs para nao voltar pro pai da DFS tree
-int dfs(int i) {
-	int lo = id[i] = p++;
+int dfs(int i, int& t) {
+	int lo = id[i] = t++;
 	s.push(i);
 	vis[i] = 2;
 
 	for (int j : g[i]) {
-		if (!vis[j]) lo = min(lo, dfs(j));
+		if (!vis[j]) lo = min(lo, dfs(j, t));
 		else if (vis[j] == 2) lo = min(lo, id[j]);
 	}
 
+	// aresta de i pro pai eh uma ponte (no caso nao direcionado)
 	if (lo == id[i]) while (1) {
 		int u = s.top(); s.pop();
 		vis[u] = 1, comp[u] = i;
@@ -29,5 +30,8 @@ int dfs(int i) {
 }
 
 void tarjan(int n) {
-	for (int i = 0; i < n; i++) if (!vis[i]) dfs(i);
+	int t = 0;
+	for (int i = 0; i < n; i++) vis[i] = 0;
+
+	for (int i = 0; i < n; i++) if (!vis[i]) dfs(i, t);
 }

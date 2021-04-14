1c94ce695846f25e017c6acf10fdc0526c3cb4f0
Bruno Monteiro <malettabruno@gmail.com>
2021-04-14 20:05:31 -0300

    Melhor dominator_points

--- Codigo/Problemas/dominatorPoints.cpp
+++ Codigo/Problemas/dominatorPoints.cpp
@@ -6,29 +6,49 @@
 // que domina p
 // insert(p) insere p no conjunto
 // (se p for dominado por alguem, nao vai inserir)
+// o multiset 'quina' guarda informacao sobre os pontos
+// nao dominados por um elemento do conjunto que nao dominam
+// outro ponto nao dominado por um elemento do conjunto
+// No caso, armazena os valores de x+y esses pontos
 //
 // Complexidades:
 // is_dominated - O(log(n))
 // insert - O(log(n)) amortizado
+// query - O(1)
 
 struct dominator_points {
 	set<pair<int, int>> se;
+	multiset<int> quina;
 
-	dominator_points() {}
 	bool is_dominated(pair<int, int> p) {
 		auto it = se.lower_bound(p);
 		if (it == se.end()) return 0;
 		return it->second >= p.second;
 	}
+	void mid(pair<int, int> a, pair<int, int> b, bool rem) {
+		pair<int, int> m = {a.first+1, b.second+1};
+		int val = m.first + m.second;
+		if (!rem) quina.insert(val);
+		else quina.erase(quina.find(val));
+	}
 	bool insert(pair<int, int> p) {
 		if (is_dominated(p)) return 0;
 		auto it = se.lower_bound(p);
+		if (it != se.begin() and it != se.end())
+			mid(*prev(it), *it, 1);
 		while (it != se.begin()) {
 			it--;
 			if (it->second > p.second) break;
+			if (it != se.begin()) mid(*prev(it), *it, 1);
 			it = se.erase(it);
 		}
-		se.insert(p);
+		it = se.insert(p).first;
+		if (it != se.begin()) mid(*prev(it), *it, 0);
+		if (next(it) != se.end()) mid(*it, *next(it), 0);
 		return 1;
+	}
+	int query() {
+		if (!quina.size()) return INF;
+		return *quina.begin();
 	}
 };

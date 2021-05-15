39c78660ee26c8258ba5a7b75373132d905b6a86
Bruno Monteiro <malettabruno@gmail.com>
2021-05-15 17:52:11 -0300

    Rmq agora retorna o indice mais a esquerda

--- Codigo/Estruturas/rmq.cpp
+++ Codigo/Estruturas/rmq.cpp
@@ -1,34 +1,34 @@
 // RMQ <O(n), O(1)> - min queue
 //
 // O(n) pra buildar, query O(1)
-// Para retornar o indice, basta
-// trocar v[...] para ... na query
+// Se tiver varios minimos, retorna
+// o de menor indice
 
 template<typename T> struct rmq {
 	vector<T> v;
 	int n; static const int b = 30;
 	vector<int> mask, t;
 
-	int op(int x, int y) { return v[x] < v[y] ? x : y; }
+	int op(int x, int y) { return v[x] <= v[y] ? x : y; }
 	int msb(int x) { return __builtin_clz(1)-__builtin_clz(x); }
 	int small(int r, int sz = b) { return r-msb(mask[r]&((1<<sz)-1)); }
+	rmq() {}
 	rmq(const vector<T>& v_) : v(v_), n(v.size()), mask(n), t(n) {
 		for (int i = 0, at = 0; i < n; mask[i++] = at |= 1) {
 			at = (at<<1)&((1<<b)-1);
-			while (at and op(i, i-msb(at&-at)) == i) at ^= at&-at;
+			while (at and op(i-msb(at&-at), i) == i) at ^= at&-at;
 		}
 		for (int i = 0; i < n/b; i++) t[i] = small(b*i+b-1);
 		for (int j = 1; (1<<j) <= n/b; j++) for (int i = 0; i+(1<<j) <= n/b; i++)
 			t[n/b*j+i] = op(t[n/b*(j-1)+i], t[n/b*(j-1)+i+(1<<(j-1))]);
 	}
-	T query(int l, int r) {
-		if (r-l+1 <= b) return v[small(r, r-l+1)];
-		int ans = op(small(l+b-1), small(r));
+	int index_query(int l, int r) {
+		if (r-l+1 <= b) return small(r, r-l+1);
 		int x = l/b+1, y = r/b-1;
-		if (x <= y) {
-			int j = msb(y-x+1);
-			ans = op(ans, op(t[n/b*j+x], t[n/b*j+y-(1<<j)+1]));
-		}
-		return v[ans];
+		if (x > y) return op(small(l+b-1), small(r));
+		int j = msb(y-x+1);
+		int ans = op(small(l+b-1), op(t[n/b*j+x], t[n/b*j+y-(1<<j)+1]));
+		return op(ans, small(r));
 	}
+	T query(int l, int r) { return v[index_query(l, r)]; }
 };

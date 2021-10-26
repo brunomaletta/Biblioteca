aa85576da36b3da14774348cccb4cd3efde8c381
Bruno Monteiro <malettabruno@gmail.com>
2021-10-26 18:03:25 -0300

    Melhoradinha no aho

--- Codigo/Strings/ahocorasick.cpp
+++ Codigo/Strings/ahocorasick.cpp
@@ -1,13 +1,15 @@
 // Aho-corasick 
 //
-// query retorna o numero de matches sem overlap
+// query retorna o somatorio do numero de matches de
+// todas as stringuinhas na stringona
 //
 // insert - O(|s| * log(SIGMA))
 // build - O(n * SIGMA), onde n = somatorio dos tamanhos das strings
+// query - O(|s|)
 
 namespace aho {
 	map<char, int> to[MAX];
-	int link[MAX], idx, term[MAX], exit[MAX];
+	int link[MAX], idx, term[MAX], exit[MAX], sobe[MAX];
 
 	void insert(string& s) {
 		int at = 0;
@@ -16,7 +18,7 @@ namespace aho {
 			if (it == to[at].end()) at = to[at][c] = ++idx;
 			else at = it->second;
 		}
-		term[at] = 1;
+		term[at]++, sobe[at]++;
 	}
 #warning nao esquece de chamar build() depois de inserir
 	void build() {
@@ -30,6 +32,7 @@ namespace aho {
 				while (l != -1 and !to[l].count(c)) l = link[l];
 				link[j] = l == -1 ? 0 : to[l][c];
 				exit[j] = term[link[j]] ? link[j] : exit[link[j]];
+				if (exit[j]+1) sobe[j] += sobe[exit[j]];
 				q.push(j);
 			}
 		}
@@ -37,9 +40,9 @@ namespace aho {
 	int query(string& s) {
 		int at = 0, ans = 0;
 		for (char c : s){
-			while (at and !to[at].count(c)) at = link[at];
-			at = to[at][c];
-			if (term[at] or exit[at]+1) ans++, at = 0;
+			while (at != -1 and !to[at].count(c)) at = link[at];
+			at = at == -1 ? 0 : to[at][c];
+			ans += sobe[at];
 		}
 		return ans;
 	}

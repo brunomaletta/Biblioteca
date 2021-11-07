d2c678c1c72df6f248a82350282cb86ec72fb397
emanueljuliano <emanueljulianoms@gmail.com>
2021-11-07 12:44:22 -0300

    interpolação linear

--- Codigo/Matematica/evalInterpol.cpp
+++ Codigo/Matematica/evalInterpol.cpp
@@ -5,23 +5,25 @@
 // por esses pontos em 'x'
 // Tudo modular, precisa do mint
 //
-// O(n log n)
+// O(n)
 
 mint evaluate_interpolation(int x, vector<mint> y) {
 	int n = y.size();
-
-	vector<mint> sulf(n+1, 1), fat(n, 1);
+	
+	vector<mint> sulf(n+1, 1), fat(n, 1), ifat(n);
 	for (int i = n-1; i >= 0; i--) sulf[i] = sulf[i+1] * (x - i);
 	for (int i = 1; i < n; i++) fat[i] = fat[i-1] * i;
+	ifat[n-1] = 1/fat[n-1];
+	for (int i = n-2; i >= 0; i--) ifat[i] = ifat[i+1] * (i + 1);
 
 	mint pref = 1, ans = 0;
 	for (int i = 0; i < n; pref *= (x - i++)) {
 		mint num = pref * sulf[i+1];
 
-		mint den = fat[i] * fat[n-1 - i];
+		mint den = ifat[i] * ifat[n-1 - i];
 		if ((n-1 - i)%2) den *= -1;
 
-		ans += y[i] * num / den;
+		ans += y[i] * num * den;
 	}
 	return ans;
 }

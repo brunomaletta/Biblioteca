f6fc4f3de6232fbb1f0fe9eef7a7ca2d452d0ffc
Bruno Monteiro <malettabruno@gmail.com>
2021-12-01 17:19:49 -0300

    Better stress

--- Codigo/Extra/stress.sh
+++ Codigo/Extra/stress.sh
@@ -1,8 +1,9 @@
-make a a2 gen || exit 1
+P=a
+make ${P} ${P}2 gen || exit 1
 for ((i = 1; ; i++)) do
 	./gen $i > in
-	./a < in > out
-	./a2 < in > out2
+	./${P} < in > out
+	./${P}2 < in > out2
 	if (! cmp -s out out2) then
 		echo "--> entrada:"
 		cat in
@@ -14,3 +15,4 @@ for ((i = 1; ; i++)) do
 	fi
 	echo $i
 done
+

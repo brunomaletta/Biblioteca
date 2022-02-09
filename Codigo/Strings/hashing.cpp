3eefc5b7a67b8fffcad3b0cdf26fa1ab151a7c5b
Bernardo Amorim <57601237+bernardoamorim@users.noreply.github.com>
2022-02-09 20:08:11 -0300

    Mudança string hashing

--- Codigo/Strings/hashing.cpp
+++ Codigo/Strings/hashing.cpp
@@ -1,38 +1,29 @@
-// String hashing
-//
-// Para evitar colisao: testar mais de um
-// mod; so comparar strings do mesmo tamanho
-// ex : str_hash<1e9+7> h(s);
-//      ll val = h(10, 20);
+// String Hashing
 //
 // Complexidades:
-// build - O(|s|)
+// construtor - O(|s|)
 // operator() - O(1)
-// 023913
+//
+// 7b7cb6
 
 mt19937 rng((int) chrono::steady_clock::now().time_since_epoch().count());
-
+ 
 int uniform(int l, int r) {
 	uniform_int_distribution<int> uid(l, r);
 	return uid(rng);
 }
-
+ 
 template<int MOD> struct str_hash {
 	static int P;
-	int n;
-	string s;
-	vector<ll> h, power;
-	str_hash(string s_) : n(s_.size()), s(s_), h(n), power(n) {
-		power[0] = 1;
-		for (int i = 1; i < n; i++) power[i] = power[i-1]*P % MOD;
-		h[0] = s[0];
-		for (int i = 1; i < n; i++) h[i] = (h[i-1]*P + s[i]) % MOD;
+	vector<ll> h, p;
+	str_hash(string s) : h(s.size()), p(s.size()) {
+		p[0] = 1, h[0] = s[0];
+		for (int i = 1; i < s.size(); i++)
+			p[i] = p[i - 1]*P%MOD, h[i] = (h[i - 1]*P + s[i])%MOD;
 	}
-	ll operator()(int i, int j) { // retorna hash da substring s[i..j]
-		if (!i) return h[j];
-		ll ret = h[j] - h[i-1]*power[j-i+1] % MOD;
-		return ret < 0 ? ret+MOD : ret;
+	ll operator()(int l, int r) { // retorna hash s[l...r]
+		ll hash = h[r] - (l ? h[l - 1]*p[r - l + 1]%MOD : 0);
+		return hash < 0 ? hash + MOD : hash;
 	}
 };
-template<int MOD> int str_hash<MOD>::P = uniform(27, MOD-1);
-// primeiro parametro deve ser maior que o tamanho do alfabeto
+template<int MOD> int str_hash<MOD>::P = uniform(256, MOD - 1); // l > |sigma|

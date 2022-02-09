f876d64e7b029b1dc854172d2d0eab3b475bc20b
Bernardo Amorim <57601237+bernardoamorim@users.noreply.github.com>
2022-02-09 20:07:47 -0300

    Mudança large hash

--- Codigo/Strings/hashingLargeMod.cpp
+++ Codigo/Strings/hashingLargeMod.cpp
@@ -1,48 +1,41 @@
-// String hashing - modulo 2^61 - 1
+// String Hashing - modulo 2^61 - 1
 //
-// Usa modulo 2^61 - 1 ~ 2e18
-// Eh quase duas vezes mais lento
+// Quase duas vezes mais lento
 //
 // Complexidades:
 // build - O(|s|)
 // operator() - O(1)
-// 418560
+//
+// a90625
 
-const ll MOD = (1ll<<61)-1;
-
+const ll MOD = (1ll<<61) - 1;
 ll mulmod(ll a, ll b) {
-	const static ll LOWER = (1ll<<30)-1, GET31 = (1ll<<31)-1;
+	const static ll LOWER = (1ll<<30) - 1, GET31 = (1ll<<31) - 1;
 	ll l1 = a&LOWER, h1 = a>>30, l2 = b&LOWER, h2 = b>>30;
 	ll m = l1*h2 + l2*h1, h = h1*h2;
 	ll ans = l1*l2 + (h>>1) + ((h&1)<<60) + (m>>31) + ((m&GET31)<<30) + 1;
-	ans = (ans&MOD) + (ans>>61);
-	ans = (ans&MOD) + (ans>>61);
-	return ans-1;
+	ans = (ans&MOD) + (ans>>61), ans = (ans&MOD) + (ans>>61);
+	return ans - 1;
 }
 
-mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count()); 
 
 ll uniform(ll l, ll r) {
-	uniform_int_distribution<ll> uid(l, r);
+	uniform_int_distribution<int> uid(l, r);
 	return uid(rng);
 }
-
+ 
 struct str_hash {
 	static ll P;
-	int n;
-	string s;
-	vector<ll> h, power;
-	str_hash(string s_) : n(s_.size()), s(s_), h(n), power(n) {
-		power[0] = 1;
-		for (int i = 1; i < n; i++) power[i] = mulmod(power[i-1], P);
-		h[0] = s[0];
-		for (int i = 1; i < n; i++) h[i] = (mulmod(h[i-1], P) + s[i]) % MOD;
+	vector<ll> h, p;
+	str_hash(string s) : h(s.size()), p(s.size()) {
+		p[0] = 1, h[0] = s[0];
+		for (int i = 1; i < s.size(); i++)
+			p[i] = mulmod(p[i - 1], P), h[i] = (mulmod(h[i - 1], P) + s[i])%MOD;
 	}
-	ll operator()(int i, int j) { // retorna hash da substring s[i..j]
-		if (!i) return h[j];
-		ll ret = h[j] - mulmod(h[i-1], power[j-i+1]);
-		return ret < 0 ? ret+MOD : ret;
+	ll operator()(int l, int r) { // retorna hash s[l...r]
+		ll hash = h[r] - (l ? mulmod(h[l - 1], p[r - l + 1]) : 0);
+		return hash < 0 ? hash + MOD : hash;
 	}
 };
-ll str_hash::P = uniform(27, MOD-1);
-// primeiro parametro deve ser maior que o tamanho do alfabeto
+ll str_hash::P = uniform(256, MOD - 1); // l > |sigma|

729203c038e161f69a3b88fd55dceed57689eba0
Emanuel <emanueljulianoms@gmail.com>
2022-10-06 15:37:10 -0300

    Corrigindo bug da virtual tree

--- Codigo/Grafos/virtualTree.cpp
+++ Codigo/Grafos/virtualTree.cpp
@@ -3,24 +3,29 @@
 // Comprime uma arvore dado um conjunto S de vertices, de forma que
 // o conjunto de vertices da arvore comprimida contenha S e seja
 // minimal e fechado sobre a operacao de LCA
-// Se |S| = k, a arvore comprimida tem O(k) vertices
+// Se |S| = k, a arvore comprimida tem menos que 2k vertices
+// As arestas de virt possuem a distancia do vertice ate o vizinho
 //
 // O(k log(k))
-// 402aff
+// 487984
 
-vector<int> virt[MAX];
+vector<pair<int, int>> virt[MAX];
 
 #warning lembrar de buildar o LCA antes
-int build_virt(vector<int> v) {
+int built_virt(vector<int> v) {
 	auto cmp = [&](int i, int j) { return lca::pos[i] < lca::pos[j]; };
 	sort(v.begin(), v.end(), cmp);
 	for (int i = v.size()-1; i; i--) v.push_back(lca::lca(v[i], v[i-1]));
 	sort(v.begin(), v.end(), cmp);
 	v.erase(unique(v.begin(), v.end()), v.end());
-	for (int i : v) virt[i].clear();
+	for (int i = 0; i < v.size(); i++) virt[v[i]].clear();
+	for (int i = 1; i < v.size(); i++) virt[lca::lca(v[i-1], v[i])].clear();
 	for (int i = 1; i < v.size(); i++) {
-#warning soh to colocando aresta descendo
-		virt[lca::lca(v[i-1], v[i])].push_back(v[i]);
+		int l = lca::lca(v[i-1], v[i]);
+		int d = lca::dist(l, v[i]);
+		virt[l].emplace_back(v[i], d);
+		virt[v[i]].emplace_back(l, d);
 	}
 	return v[0];
 }
+

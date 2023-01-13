e474d38f2645df1097f2d5e239f4fb78b1d63cd5
Kaio Vieira <kaiomassevieira@gmail.com>
2023-01-13 20:18:33 -0300

    Fix tabs no convex_hull

--- Codigo/Primitivas/geometriaInt.cpp
+++ Codigo/Primitivas/geometriaInt.cpp
@@ -143,23 +143,23 @@ int inpol(vector<pt>& v, pt p) { // O(n)
 
 // 10d7e0
 vector<pt> convex_hull(vector<pt> v) { // convex hull - O(n log(n))
-    sort(v.begin(), v.end());
-    v.erase(unique(v.begin(), v.end()), v.end());
-    if (v.size() <= 1) return v;
-    vector<pt> l, u;
-    for (int i = 0; i < v.size(); i++) {
-        while (l.size() > 1 and !ccw(l.end()[-2], l.end()[-1], v[i]))
-            l.pop_back();
-        l.push_back(v[i]);
-    }
-    for (int i = v.size() - 1; i >= 0; i--) {
-        while (u.size() > 1 and !ccw(u.end()[-2], u.end()[-1], v[i]))
-            u.pop_back();
-        u.push_back(v[i]);
-    }
-    l.pop_back(); u.pop_back();
-    for (pt i : u) l.push_back(i);
-    return l;
+	sort(v.begin(), v.end());
+	v.erase(unique(v.begin(), v.end()), v.end());
+	if (v.size() <= 1) return v;
+	vector<pt> l, u;
+	for (int i = 0; i < v.size(); i++) {
+		while (l.size() > 1 and !ccw(l.end()[-2], l.end()[-1], v[i]))
+			l.pop_back();
+		l.push_back(v[i]);
+	}
+	for (int i = v.size() - 1; i >= 0; i--) {
+		while (u.size() > 1 and !ccw(u.end()[-2], u.end()[-1], v[i]))
+			u.pop_back();
+		u.push_back(v[i]);
+	}
+	l.pop_back(); u.pop_back();
+	for (pt i : u) l.push_back(i);
+	return l;
 }
 
 // af2d96

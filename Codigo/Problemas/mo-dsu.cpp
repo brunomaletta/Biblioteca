03587d9b75ae4d53b411dc9d78598e7ee3e5262a
Kaio Vieira <kaiomassevieira@gmail.com>
2023-01-13 23:17:27 -0300

    Fixed mo-dsu quadratico

--- Codigo/Problemas/mo-dsu.cpp
+++ Codigo/Problemas/mo-dsu.cpp
@@ -6,7 +6,7 @@
 // Da pra adaptar pra usar MO com qualquer estrutura rollbackavel
 //
 // O(m sqrt(q) log(n))
-// f98540
+// 704722
 
 struct dsu {
 	int n, ans;
@@ -41,18 +41,29 @@ struct dsu {
 };
 
 int n;
-vector<pair<int, int>> ar; // vetor com as arestas
+vector<pair<int, int>> ar;
 
+// 9d242b
 vector<int> MO(vector<pair<int, int>> &q) {
-	int SQ = ar.size() / sqrt(q.size()) + 1;
+	int SQ = sqrt(q.size()) + 1;
 	int m = q.size();
 	vector<int> ord(m);
 	iota(ord.begin(), ord.end(), 0);
 	sort(ord.begin(), ord.end(), [&](int l, int r) {
-		if (q[l].first / SQ != q[r].first / SQ) return q[l].first < q[r].first;
-		return q[l].second < q[r].second;
-	});
+			if (q[l].first / SQ != q[r].first / SQ) return q[l].first < q[r].first;
+			return q[l].second < q[r].second;
+			});
 	vector<int> ret(m);
+
+	dsu small(n);
+	for (int i = 0; i < m; i++) {
+		auto [l, r] = q[ord[i]];
+		if (l / SQ == r / SQ) {
+			for (int k = l; k <= r; k++) small.add(ar[k]);
+			ret[ord[i]] = small.query();
+			for (int k = l; k <= r; k++) small.rollback();
+		}
+	}
 
 	for (int i = 0; i < m; i++) {
 		dsu D(n);
@@ -62,12 +73,7 @@ vector<int> MO(vector<pair<int, int>> &q) {
 		while (j+1 < m and q[ord[j+1]].first / SQ == q[ord[i]].first / SQ) {
 			auto [l, r] = q[ord[++j]];
 
-			if (l / SQ == r / SQ) {
-				dsu D2(n);
-				for (int k = l; k <= r; k++) D2.add(ar[k]);
-				ret[ord[j]] = D2.query();
-				continue;
-			}
+			if (l / SQ == r / SQ) continue;
 
 			while (last_r < r) D.add(ar[++last_r]);
 			for (int k = l; k <= fim; k++) D.add(ar[k]);

7c528f55d5f5652a0c67705d14280593eaaf2bc0
Bruno Monteiro <malettabruno@gmail.com>
2023-01-30 23:15:42 -0300

    Nome correto do cara

--- Codigo/Grafos/lowerBoundMaxFlow.cpp
+++ Codigo/Grafos/lowerBoundMaxFlow.cpp
@@ -5,19 +5,19 @@
 // add(a, b, c):
 // 	adiciona aresta de a pra b com capacidade c
 //
-// Mesma complexidade do Dinic
+// Mesma complexidade do Dinitz
 // 5f2379
 
-struct lb_max_flow : dinic {
+struct lb_max_flow : dinitz {
 	vector<int> d;
-	lb_max_flow(int n) : dinic(n + 2), d(n, 0) {}
+	lb_max_flow(int n) : dinitz(n + 2), d(n, 0) {}
 	void add(int a, int b, int l, int r) {
 		d[a] -= l;
 		d[b] += l;
-		dinic::add(a, b, r - l);
+		dinitz::add(a, b, r - l);
 	}
 	void add(int a, int b, int c) {
-		dinic::add(a, b, c);
+		dinitz::add(a, b, c);
 	}
 	bool has_circulation() {
 		int n = d.size();
@@ -26,21 +26,21 @@ struct lb_max_flow : dinic {
 		for (int i = 0; i < n; i++) {
 			if (d[i] > 0) {
 				cost += d[i];
-				dinic::add(n, i, d[i]);
+				dinitz::add(n, i, d[i]);
 			} else if (d[i] < 0) {
-				dinic::add(i, n+1, -d[i]);
+				dinitz::add(i, n+1, -d[i]);
 			}
 		}
 
-		return (dinic::max_flow(n, n+1) == cost);
+		return (dinitz::max_flow(n, n+1) == cost);
 	}
 	bool has_flow(int src, int snk) {
-		dinic::add(snk, src, INF);
+		dinitz::add(snk, src, INF);
 		return has_circulation();
 	}
 	ll max_flow(int src, int snk) {
 		if (!has_flow(src, snk)) return -1;
-		dinic::F = 0;
-		return dinic::max_flow(src, snk);
+		dinitz::F = 0;
+		return dinitz::max_flow(src, snk);
 	}
 };

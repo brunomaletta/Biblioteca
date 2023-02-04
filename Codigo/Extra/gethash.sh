f96b72b1c765773cafea81fa5d873b17907aed14
Bruno Monteiro <malettabruno@gmail.com>
2023-02-04 00:12:57 -0300

    Fix gethash

--- Codigo/Extra/gethash.sh
+++ Codigo/Extra/gethash.sh
@@ -2,8 +2,8 @@
 # bash gethash.sh arquivo.cpp
 echo "" > pref.txt
 while IFS= read -r l; do
-	echo $l >> pref.txt
-	echo $l > line.txt
+	echo "$l" >> pref.txt
+	echo "$l" > line.txt
 	hp=$(echo $(bash hash.sh pref.txt 1 1000) | cut -c-2)
 	hl=$(echo $(bash hash.sh line.txt 1 1000) | cut -c-2)
 	echo -e "$hp $hl $l"

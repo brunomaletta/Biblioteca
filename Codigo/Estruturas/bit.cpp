630941e90e409a8ad622fffa062ef8ee788193ad
Kaio Vieira <kaiomassevieira@gmail.com>
2023-08-29 20:54:59 -0300

    Adicionei struct na BIT, e removi o método põe

--- Codigo/Estruturas/bit.cpp
+++ Codigo/Estruturas/bit.cpp
@@ -1,51 +1,43 @@
 // BIT
 //
-// BIT de soma 1-based, v 0-based
-// Para mudar o valor da posicao p para x,
-// faca: poe(x - query(p, p), p)
-// l_bound(x) retorna o menor p tal que
-// query(1, p+1) > x    (0 based!)
+// BIT de soma 0-based
+// 
+// upper_bound(x) retorna o menor p tal que
+// query(1, p+1) > x 
 //
 // Complexidades:
 // build - O(n)
-// poe - O(log(n))
+// update - O(log(n))
 // query - O(log(n))
-// l_bound - O(log(n))
+// upper_bound - O(log(n))
 
-int n;
-int bit[MAX];
-int v[MAX];
-
-void build() {
-	bit[0] = 0;
-	for (int i = 1; i <= n; i++) bit[i] = v[i - 1];
-
-	for (int i = 1; i <= n; i++) {
-		int j = i + (i & -i);
-		if (j <= n) bit[j] += bit[i];
+struct Bit {
+	int n;
+	vector<ll> bit;
+	Bit(int _n=0) : n(_n), bit(n + 1) {}
+	Bit(vector<int>& v) : n(v.size()), bit(n + 1) {
+		for (int i = 1; i <= n; i++) {
+			bit[i] += v[i - 1];
+			int j = i + (i & -i);
+			if (j <= n) bit[j] += bit[i];
+		}
 	}
-}
-
-// soma x na posicao p
-void poe(int x, int p) {
-	for (; p <= n; p += p & -p) bit[p] += x;
-}
-
-// soma [1, p]
-int pref(int p) {
-	int ret = 0;
-	for (; p; p -= p & -p) ret += bit[p];
-	return ret;
-}
-
-// soma [a, b]
-int query(int a, int b) {
-	return pref(b) - pref(a - 1);
-}
-
-int l_bound(ll x) {
-	int p = 0;
-	for (int i = MAX2; i+1; i--) if (p + (1<<i) <= n
-		and bit[p + (1<<i)] <= x) x -= bit[p += (1<<i)];
-	return p;
-}
+	void update(int i, ll x) { // soma x na posicao i
+		for (i++; i <= n; i += i & -i) bit[i] += x;
+	}
+	ll pref(int i) { // soma [0, i]
+		ll ret = 0;
+		for (i++; i; i -= i & -i) ret += bit[i];
+		return ret;
+	}
+	ll query(int l, int r) {  // soma [l, r]
+		return pref(r) - pref(l - 1); 
+	}
+	int upper_bound(ll x) {
+		int p = 0;
+		for (int i = __lg(n); i+1; i--) 
+			if (p + (1<<i) <= n and bit[p + (1<<i)] <= x)
+				x -= bit[p += (1 << i)];
+		return p;
+	}
+};

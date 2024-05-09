e4b4e1e8f6b0ff0c3f02cc3ea04a4d4e36e3214b
luizacbcampos <luizacbcampos@gmail.com>
2024-05-09 18:32:29 -0300

    update docs generation script

--- docs/gendocs.cpp
+++ docs/gendocs.cpp
@@ -7,6 +7,7 @@ using namespace std;
 #define RED "\033[0;31m"
 #define RESET "\033[0m"
 #define CYAN "\033[0;36m"
+#define GREEN "\033[0;32m"
 #define BOLD "\033[1m"
 #define NO_BOLD "\033[0m"
 
@@ -30,6 +31,7 @@ string hash_cmd = "sed -n 1','10000' p' tmp.cpp | sed '/^#w/d' "
 
 bool print_all = false;
 bool overwrite = false;
+bool update = false;
 
 void strip(string& str) {
     if (str.length() == 0) return;
@@ -197,6 +199,90 @@ string get_original_link(string path_code) {
 	return link + path_code;
 }
 
+pair<int, int> get_code_range(string path_docs) {
+	ifstream in(path_docs);
+	string line;
+	vector<string> lines;
+	pair<int, int> code_range;
+	// Read all lines
+	while (getline(in, line)) {
+		lines.push_back(line);
+	}
+	in.close();
+
+	// If line[i] == "##Código", and line[i+1] == "```cpp", then read all lines until line[i] == "```"
+	for (int i = 0; i < lines.size(); i++) {
+		if (lines[i] == "## Código" and lines[i + 1] == "```cpp") {
+			code_range.first = i + 1;
+			while (i < lines.size() and lines[i] != "```") i++;
+			code_range.second = i;
+			break;
+		}
+	}
+
+	return code_range;
+}
+
+pair<int, int> find_dashed_lines(vector<string>& lines) {
+	pair<int, int> dashed_lines;
+	bool found_first_dashed_line = false;
+	for (int i = 0; i < lines.size(); i++) {
+		if (lines[i].size() == 3 and lines[i][0] == '-' and lines[i][1] == '-' and lines[i][2] == '-') {
+			if (!found_first_dashed_line) {
+				dashed_lines.first = i;
+				found_first_dashed_line = true;
+			}
+			else {
+				dashed_lines.second = i;
+				break;
+			}
+		}
+	}
+	return dashed_lines;
+}
+
+void update_existing_file(string path_docs, string path_code, string title) {
+	// Updates the code portion of the file
+	// pair < comments, code >
+	pair<vector<string>, vector<string>> code = get_code(path_code);
+
+	// Get the range of the code portion
+	pair<int, int> code_range = get_code_range(path_docs);
+
+	string line;
+	vector<string> old_lines;
+	// Read the file as it is now
+	ifstream in(path_docs);
+	while (getline(in, line))
+		old_lines.push_back(line);
+	in.close();
+
+	pair<int, int> dashed_lines = find_dashed_lines(old_lines);
+	
+	// Update the date portion in the dash lines
+	for (int i = dashed_lines.first; i < dashed_lines.second; i++) {
+		if (old_lines[i].rfind("date: \"", 0) == 0) {
+			old_lines[i] = "date: \"" + time_to_string(get_current_time()) + "\""; // Update the date
+		}
+	}
+
+	// Update the code portion
+	ofstream out(path_docs);
+	for (int i = 0; i < dashed_lines.first; i++) out << old_lines[i] << endl; // Write everything before the dashed lines
+
+	for (int i = dashed_lines.first; i < dashed_lines.second; i++) out << old_lines[i] << endl; // Write the dashed lines
+
+	for (int i = dashed_lines.second; i < code_range.first; i++) out << old_lines[i] << endl; // Write everything between the dashed lines and the code portion
+
+	out << "```cpp" << endl; // Start of the code portion
+	for (auto line : code.second) out << line << endl; // Write the new code
+	out << "```" << endl;
+	
+	for (int i = code_range.second + 1; i < old_lines.size(); i++) out << old_lines[i] << endl; // Write everything after the code portion
+	out.close();
+
+}
+
 void create_code_file(string path_docs, string path_code, string title) {
 	// pair < comments, code >
 	pair<vector<string>, vector<string>> code = get_code(path_code);
@@ -249,9 +335,10 @@ void create_default_index(string path_index, string index_title) {
 	out.close();
 }
 
-void create_section(string path) {
+void create_section(string path, string dir = docs_path) {
 	// Check if directory exists
-	string dir = docs_path + path;
+	dir += path;
+
 	if (fs::is_directory(fs::path(dir))){
 		// Check if dir + '/_index.md' exists
 		string index = dir + "/_index.md";
@@ -269,12 +356,29 @@ void create_section(string path) {
 	}
 }
 
+void create_directory(string path_docs) {
+	const fs::path shorten(path_docs);
+	fs::path directory = shorten.parent_path();
+
+	while (!directory.empty()) {
+		if (!fs::exists(directory)) {
+			// std::cerr << "Directory " << directory.string() << " does not exist." << std::endl;
+			create_section(directory.string(), "");
+		}
+		directory = directory.parent_path();
+	}
+}
+
 int main(int argc, char** argv) {
 	if (argc > 1) {
 		string arg1(argv[1]);
 		if (arg1 == "--overwrite") {
 			overwrite = true;
 			cerr << "Rewriting all docs files..." << endl << endl;
+		}
+		else if (arg1 == "--update") {
+			update = true;
+			cerr << "Updating all docs files..." << endl << endl;
 		}
 	}
 
@@ -295,28 +399,39 @@ int main(int argc, char** argv) {
 		
 		// print all files
 		for (auto [f, f_path] : files) {
+			// const fs::path path = inputPath;
 			string title = get_name(f_path);
 			strip(title);
 			string path_docs = get_docs_path(f_path);
 			cout << CYAN << "\t" << title << RESET << endl;
-			cerr << "\t" << f_path << " -> " << path_docs << endl;
+			
+			create_directory(path_docs);
+
+
 
 			if (overwrite) {
 				create_code_file(path_docs, f_path, title);
+				cerr << "\t" << f_path << " -> " << path_docs << endl;
+				continue;
 			}
-			else{
-				if (!fs::exists(fs::path(path_docs))) {
-					create_code_file(path_docs, f_path, title);
+			
+			if (!fs::exists(fs::path(path_docs))) {
+				create_code_file(path_docs, f_path, title);
+				cerr << "\t" << f_path << " -> " << path_docs << endl;
+			}
+			else {
+				if (update) {
+					update_existing_file(path_docs, f_path, title);
+					cerr << "\t" << GREEN << "UPDATING: "<< RESET << f_path << " -> " << path_docs << endl;
 				}
-				else {
-					cerr << "File already exists: " << path_docs << endl;
-				}
+				else
+					cerr << "\tFile already exists" << path_docs << endl;
 			}
 		}
 	}
 
 	// printa_section("Extra");
-	vector<pair<string, string>> files;
+	// vector<pair<string, string>> files;
 	// dfs(files, path + "Extra", true);
 
 	return 0;
